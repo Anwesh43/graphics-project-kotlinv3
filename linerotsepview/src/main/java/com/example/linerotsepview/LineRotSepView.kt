fafb387091c29c75f92f358cc3b98e7892009f99
Anwesh43 <anweshthecool0@gmail.com>
2023-05-14 01:16:42 +0530

    linerotsepview: adding LineRotSepView class

--- linerotsepview/src/main/java/com/example/linerotsepview/LineRotSepView.kt
+++ linerotsepview/src/main/java/com/example/linerotsepview/LineRotSepView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLRSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotSep(scale, w, h, paint)
 }
+
+class LineRotSepView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
