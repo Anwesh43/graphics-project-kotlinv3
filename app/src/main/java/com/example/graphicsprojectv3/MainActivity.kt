8dbe9a7fc33547172f00c5100d6a5c9a6d869048
Anwesh43 <anweshthecool0@gmail.com>
2023-05-14 01:39:23 +0530

    linerotsepview: adding fixes

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.graphicsprojectv3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.linerotsepview.LineRotSepView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        LineRotSepView.create(this)
     }
 }
