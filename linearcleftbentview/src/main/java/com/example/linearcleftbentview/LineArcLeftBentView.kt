5c2d4a23065bf40299e912915703775baa3c2299
Anwesh43 <anweshthecool0@gmail.com>
2023-05-16 08:29:00 +0530

    linearcleftbentview: adding LineArcLeftBar class

--- linearcleftbentview/src/main/java/com/example/linearcleftbentview/LineArcLeftBentView.kt
+++ linearcleftbentview/src/main/java/com/example/linearcleftbentview/LineArcLeftBentView.kt
@@ -163,4 +163,27 @@ class LineArcLeftBentView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineArcLeftBent(var i : Int) {
+
+        private var curr : LALBNode = LALBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUdpating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
