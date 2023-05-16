492d58f1425501aeec175bba4c67c6531f726b46
Anwesh43 <anweshthecool0@gmail.com>
2023-05-16 08:36:13 +0530

    linearcleftbentview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linebentsqrightview.LineBentSqRightView
+import com.example.linearcleftbentview.LineArcLeftBentView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineBentSqRightView.create(this)
+        LineArcLeftBentView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
