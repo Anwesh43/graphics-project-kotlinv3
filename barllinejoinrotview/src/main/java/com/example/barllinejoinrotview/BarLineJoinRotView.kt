c12c976f0f39f60529bf61f2154a26b07f80ef0d
Anwesh43 <anweshthecool0@gmail.com>
2023-05-17 17:18:13 +0530

    barllinejoinrotview: adding BarLineJoinRotView class

--- barllinejoinrotview/src/main/java/com/example/barllinejoinrotview/BarLineJoinRotView.kt
+++ barllinejoinrotview/src/main/java/com/example/barllinejoinrotview/BarLineJoinRotView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBLJRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBarLineJoinRot(scale, w, h, paint)
 }
+
+class BarLineJoinRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
