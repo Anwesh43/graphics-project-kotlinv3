c41846b7f25297f732d4cdf41fc2ec5d5dad6618
Anwesh43 <anweshthecool0@gmail.com>
2023-05-17 17:24:23 +0530

    barllinejoinrotview: adding BLJRNode class

--- barllinejoinrotview/src/main/java/com/example/barllinejoinrotview/BarLineJoinRotView.kt
+++ barllinejoinrotview/src/main/java/com/example/barllinejoinrotview/BarLineJoinRotView.kt
@@ -124,4 +124,45 @@ class BarLineJoinRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLJRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLJRNode? = null
+        private var prev : BLJRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLJRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLJRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLJRNode {
+            var curr : BLJRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
