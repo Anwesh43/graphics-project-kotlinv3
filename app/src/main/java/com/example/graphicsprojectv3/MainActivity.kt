f94f164ad053381f4446202a418221681b6ef0cd
Anwesh43 <anweshthecool0@gmail.com>
2023-05-17 18:03:17 +0530

    barllinejoinrotview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linearcleftbentview.LineArcLeftBentView
+import com.example.barllinejoinrotview.BarLineJoinRotView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineArcLeftBentView.create(this)
+        BarLineJoinRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
