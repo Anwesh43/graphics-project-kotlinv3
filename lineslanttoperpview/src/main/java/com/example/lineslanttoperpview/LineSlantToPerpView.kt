f7b375370e0c4daede8cbeb2a4757773448b67ca
Anwesh43 <anweshthecool0@gmail.com>
2023-05-18 11:24:57 +0530

    lineslanttoperpview: adding LSTPNode class

--- lineslanttoperpview/src/main/java/com/example/lineslanttoperpview/LineSlantToPerpView.kt
+++ lineslanttoperpview/src/main/java/com/example/lineslanttoperpview/LineSlantToPerpView.kt
@@ -122,4 +122,45 @@ class LineSlantToPerpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSTPNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LSTPNode? = null
+        private var prev : LSTPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSTPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSTPNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSTPNode {
+            var curr : LSTPNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
