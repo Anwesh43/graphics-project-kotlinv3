6f4447eae9d9816fe9d64a1da2bf405b23ac4bce
Anwesh43 <anweshthecool0@gmail.com>
2023-05-19 02:28:26 +0530

    blockrotjoindownview: adding BlockRotJoinDownView class

--- blockrotjoindownview/src/main/java/com/example/blockrotjoindownview/BlockRotJoinDownView.kt
+++ blockrotjoindownview/src/main/java/com/example/blockrotjoindownview/BlockRotJoinDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBRJDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBlockRotJoinDown(scale, w, h, paint)
 }
+
+class BlockRotJoinDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
