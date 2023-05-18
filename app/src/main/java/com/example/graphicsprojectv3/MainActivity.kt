f748ebd25e583010c7a0b6dd822945a55a00c2e8
Anwesh43 <anweshthecool0@gmail.com>
2023-05-18 11:47:48 +0530

    lineslanttoperpview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.barllinejoinrotview.BarLineJoinRotView
+import com.example.lineslanttoperpview.LineSlantToPerpView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BarLineJoinRotView.create(this)
+        LineSlantToPerpView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
