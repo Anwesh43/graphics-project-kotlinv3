a0adfe01b72a9111f89f30a6b4889949a08cf83d
Anwesh43 <anweshthecool0@gmail.com>
2023-05-19 02:32:52 +0530

    blockrotjoindownview: adding BRJDNode class

--- blockrotjoindownview/src/main/java/com/example/blockrotjoindownview/BlockRotJoinDownView.kt
+++ blockrotjoindownview/src/main/java/com/example/blockrotjoindownview/BlockRotJoinDownView.kt
@@ -125,4 +125,45 @@ class BlockRotJoinDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BRJDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BRJDNode? = null
+        private var prev : BRJDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BRJDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBRJDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BRJDNode {
+            var curr : BRJDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
