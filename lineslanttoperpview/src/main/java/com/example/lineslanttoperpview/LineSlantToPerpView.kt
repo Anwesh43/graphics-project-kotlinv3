29b99837eb1fb475dac6d9bed73350f465424ffe
Anwesh43 <anweshthecool0@gmail.com>
2023-05-18 11:26:28 +0530

    lineslanttoperpview: adding LineSlantToPerp class

--- lineslanttoperpview/src/main/java/com/example/lineslanttoperpview/LineSlantToPerpView.kt
+++ lineslanttoperpview/src/main/java/com/example/lineslanttoperpview/LineSlantToPerpView.kt
@@ -163,4 +163,27 @@ class LineSlantToPerpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineSlantToPerp(var i : Int) {
+
+        private var curr : LSTPNode = LSTPNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
