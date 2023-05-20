95c88d65c6dec0f9de1ac7d5d17efabe6ae15e5d
Anwesh43 <anweshthecool0@gmail.com>
2023-05-20 14:15:04 +0530

    linedividearcunderview : adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.blockrotjoindownview.BlockRotJoinDownView
+import com.example.linedividearcunderview.LineDivideArcUnderView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BlockRotJoinDownView.create(this)
+        LineDivideArcUnderView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
