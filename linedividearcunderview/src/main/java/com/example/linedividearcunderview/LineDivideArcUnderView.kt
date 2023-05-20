64c974b4445eddb8bfdc000bbb97f17a4b5a506d
Anwesh43 <anweshthecool0@gmail.com>
2023-05-20 13:38:58 +0530

    linedividearcunderview: adding Canvas extension methods

--- linedividearcunderview/src/main/java/com/example/linedividearcunderview/LineDivideArcUnderView.kt
+++ linedividearcunderview/src/main/java/com/example/linedividearcunderview/LineDivideArcUnderView.kt
@@ -24,8 +24,42 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineDivideArcUnder(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(rot * dsc(2))
+                drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+                drawArc(RectF(-size, -size / 2, 0f, size / 2), 0f, 180f * dsc(1), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLDAUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineDivideArcUnder(scale, w, h, paint)
+}
