7cbeffdd3d56378772288b02da8130e4f43bec77
Anwesh43 <anweshthecool0@gmail.com>
2023-05-20 13:40:18 +0530

    linedividearcunderview : adding LineDivideArcUnderView class

--- linedividearcunderview/src/main/java/com/example/linedividearcunderview/LineDivideArcUnderView.kt
+++ linedividearcunderview/src/main/java/com/example/linedividearcunderview/LineDivideArcUnderView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLDAUNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineDivideArcUnder(scale, w, h, paint)
 }
+
+class LineDivideArcUnderView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
