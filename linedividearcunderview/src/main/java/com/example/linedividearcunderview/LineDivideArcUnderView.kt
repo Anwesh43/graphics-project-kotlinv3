a925398290f635e30a1639a4c4594493b92da0af
Anwesh43 <anweshthecool0@gmail.com>
2023-05-20 13:47:39 +0530

    linedividearcunderview : adding LDAUNode class

--- linedividearcunderview/src/main/java/com/example/linedividearcunderview/LineDivideArcUnderView.kt
+++ linedividearcunderview/src/main/java/com/example/linedividearcunderview/LineDivideArcUnderView.kt
@@ -126,4 +126,45 @@ class LineDivideArcUnderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDAUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDAUNode? = null
+        private var prev : LDAUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDAUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDAUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDAUNode {
+            var curr : LDAUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
