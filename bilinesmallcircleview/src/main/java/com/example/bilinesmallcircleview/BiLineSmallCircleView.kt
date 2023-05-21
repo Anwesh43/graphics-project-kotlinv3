9b605242d5816a5ef8b8f9c550786aecefe71ef0
Anwesh43 <anweshthecool0@gmail.com>
2023-05-21 15:34:48 +0530

    bilinesmallcircleview: adding constants and libraries

---
+++ bilinesmallcircleview/src/main/java/com/example/bilinesmallcircleview/BiLineSmallCircleView.kt
@@ -0,0 +1,28 @@
+package com.example.bilinesmallcircleview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val strokeFactor : Float = 90f
+val sizeFactor  : Float = 4.9f
+
