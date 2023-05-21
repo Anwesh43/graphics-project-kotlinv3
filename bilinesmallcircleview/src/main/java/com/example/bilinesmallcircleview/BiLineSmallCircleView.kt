8a944bdeb1671f5f8dba6e2987440b369fb4cbd7
Anwesh43 <anweshthecool0@gmail.com>
2023-05-21 15:52:44 +0530

    bilinesmallcircleview: adding BLSCNode class

--- bilinesmallcircleview/src/main/java/com/example/bilinesmallcircleview/BiLineSmallCircleView.kt
+++ bilinesmallcircleview/src/main/java/com/example/bilinesmallcircleview/BiLineSmallCircleView.kt
@@ -127,4 +127,45 @@ class BiLineSmallCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLSCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLSCNode? = null
+        private var prev : BLSCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLSCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLSCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLSCNode {
+            var curr : BLSCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
