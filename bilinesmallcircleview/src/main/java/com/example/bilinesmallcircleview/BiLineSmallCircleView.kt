f39460e8dd9533a85edaeb52488d5535212b9c3d
Anwesh43 <anweshthecool0@gmail.com>
2023-05-21 15:45:23 +0530

    bilinesmallcircleview: adding BiLineSamllCircleView

--- bilinesmallcircleview/src/main/java/com/example/bilinesmallcircleview/BiLineSmallCircleView.kt
+++ bilinesmallcircleview/src/main/java/com/example/bilinesmallcircleview/BiLineSmallCircleView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawBLSCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineSmallCircle(scale, w, h, paint)
 }
+
+class BiLineSmallCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
