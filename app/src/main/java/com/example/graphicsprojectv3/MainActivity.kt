24cc403fb2e5c013c3eb14ffef86079be0fd9912
Anwesh43 <anweshthecool0@gmail.com>
2023-05-21 20:50:07 +0530

    bilinesmallcircleview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linedividearcunderview.LineDivideArcUnderView
+import com.example.bilinesmallcircleview.BiLineSmallCircleView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineDivideArcUnderView.create(this)
+        BiLineSmallCircleView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
