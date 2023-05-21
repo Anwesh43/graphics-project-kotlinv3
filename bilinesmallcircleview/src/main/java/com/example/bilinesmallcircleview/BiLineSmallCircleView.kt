0a82dbda254bc5b5e712b1c3054ca9e1d858b7fc
Anwesh43 <anweshthecool0@gmail.com>
2023-05-21 15:35:50 +0530

    bilinesmallcircleview: adding Int and Float extension methods

--- bilinesmallcircleview/src/main/java/com/example/bilinesmallcircleview/BiLineSmallCircleView.kt
+++ bilinesmallcircleview/src/main/java/com/example/bilinesmallcircleview/BiLineSmallCircleView.kt
@@ -26,3 +26,6 @@ val delay : Long = 20
 val strokeFactor : Float = 90f
 val sizeFactor  : Float = 4.9f
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
