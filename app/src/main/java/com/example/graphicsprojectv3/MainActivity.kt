1e8de7e348e9cfe4dce2fbe86f149ea53c94d97f
Anwesh43 <anweshthecool0@gmail.com>
2023-05-23 03:40:16 +0530

    linerotintorotlineview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.tbentlinedownview.TBentLineDownView
+import com.example.linerotintolineview.LineRotIntoLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        TBentLineDownView.create(this)
+        LineRotIntoLineView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
