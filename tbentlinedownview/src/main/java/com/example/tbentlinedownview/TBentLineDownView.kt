6b681f6aca21582e6fb1a911754c23efe2873865
Anwesh43 <anweshthecool0@gmail.com>
2023-05-22 09:43:24 +0530

    tbentlinedownview: adding TBLDNode class

--- tbentlinedownview/src/main/java/com/example/tbentlinedownview/TBentLineDownView.kt
+++ tbentlinedownview/src/main/java/com/example/tbentlinedownview/TBentLineDownView.kt
@@ -123,4 +123,45 @@ class TBentLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TBLDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : TBLDNode? = null
+        private var prev : TBLDNode?= null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TBLDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTBLDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TBLDNode {
+            var curr : TBLDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
