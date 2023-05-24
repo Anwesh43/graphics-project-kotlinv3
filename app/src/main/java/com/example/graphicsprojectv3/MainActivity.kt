884bcff7f8274301fa6454247119393a24a65cf8
Anwesh43 <anweshthecool0@gmail.com>
2023-05-24 17:33:36 +0530

    arcbentlinerightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotintolineview.LineRotIntoLineView
+import com.example.arcbentlinerightview.ArcBentLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineRotIntoLineView.create(this)
+        ArcBentLineView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
