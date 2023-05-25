de3060abb534d403586d7e34990285231f340f24
Anwesh43 <anweshthecool0@gmail.com>
2023-05-25 12:36:52 +0530

    arrowbiarcview: adding ArrowBiArcLineView class

--- arrowbiarcview/src/main/java/com/example/arrowbiarcview/ArrowBiArcLineView.kt
+++ arrowbiarcview/src/main/java/com/example/arrowbiarcview/ArrowBiArcLineView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawABALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArrowBiArcLine(scale, w, h, paint)
 }
+
+class ArrowBiArcLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
