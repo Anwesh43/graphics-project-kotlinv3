88b344d5860db3e42605eaba769dcfa7a9c90512
Anwesh43 <anweshthecool0@gmail.com>
2023-05-25 15:19:07 +0530

    arrowbiarcview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.arcbentlinerightview.ArcBentLineView
+import com.example.arrowbiarcview.ArrowBiArcLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        ArcBentLineView.create(this)
+        ArrowBiArcLineView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
