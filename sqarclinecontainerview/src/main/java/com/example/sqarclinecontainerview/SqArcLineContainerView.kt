96b372b30bef751802f3fdf16f4b37125edaa552
Anwesh43 <anweshthecool0@gmail.com>
2023-05-26 07:09:43 +0530

    sqarclinecontainerview: adding Canvas extension methods

--- sqarclinecontainerview/src/main/java/com/example/sqarclinecontainerview/SqArcLineContainerView.kt
+++ sqarclinecontainerview/src/main/java/com/example/sqarclinecontainerview/SqArcLineContainerView.kt
@@ -29,3 +29,33 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqArcLineContainer(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        drawLine(-size, 0f, -size + size * dsc(0), 0f, paint)
+        drawArc(RectF(0f, -size / 2, size, size / 2), rot - rot * dsc(1), rot * dsc(1), true, paint)
+        drawXY(size * 0.9f, -h /2 + (h / 2) * dsc(2)) {
+            drawRect(RectF(0f, -size / 10, size / 10, 0f), paint)
+        }
+    }
+}
+
+fun Canvas.drawSALCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSqArcLineContainer(scale, w, h, paint)
+}
