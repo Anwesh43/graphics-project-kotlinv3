166ff7f23b73d3d3c7f810222686c2e019fabc3c
Anwesh43 <anweshthecool0@gmail.com>
2023-05-27 01:43:50 +0530

    sqrotshiftdownview: adding SRSDNode class

--- sqrotshiftdownview/src/main/java/com/example/sqrotshiftdownview/SqRotShiftDownView.kt
+++ sqrotshiftdownview/src/main/java/com/example/sqrotshiftdownview/SqRotShiftDownView.kt
@@ -116,4 +116,45 @@ class SqRotShiftDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SRSDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SRSDNode? = null
+        private var prev : SRSDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SRSDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSRSDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SRSDNode {
+            var curr : SRSDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
