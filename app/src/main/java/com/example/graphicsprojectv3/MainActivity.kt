ef2d344c101f888f8261032c0be18407c340cfb8
Anwesh43 <anweshthecool0@gmail.com>
2023-05-27 01:50:43 +0530

    sqrotshiftdownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqarclinecontainerview.SqArcLineContainerView
+import com.example.sqrotshiftdownview.SqRotShiftDownView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SqArcLineContainerView.create(this)
+        SqRotShiftDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
