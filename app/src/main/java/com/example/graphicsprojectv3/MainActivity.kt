a7a04ba076481a3647d71dafd570d237ffd54b76
Anwesh43 <anweshthecool0@gmail.com>
2023-05-26 07:35:55 +0530

    sqarclinecontainerview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.arrowbiarcview.ArrowBiArcLineView
+import com.example.sqarclinecontainerview.SqArcLineContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        ArrowBiArcLineView.create(this)
+        SqArcLineContainerView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
