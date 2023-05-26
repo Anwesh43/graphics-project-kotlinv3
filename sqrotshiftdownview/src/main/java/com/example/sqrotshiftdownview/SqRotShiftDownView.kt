50ea475d3d376a3f7bca4786e30f239aeecd7c0b
Anwesh43 <anweshthecool0@gmail.com>
2023-05-27 01:36:27 +0530

    sqrotshiftdownview: adding SqRotShifDownView class

--- sqrotshiftdownview/src/main/java/com/example/sqrotshiftdownview/SqRotShiftDownView.kt
+++ sqrotshiftdownview/src/main/java/com/example/sqrotshiftdownview/SqRotShiftDownView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawSRSDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawSqRotShiftDown(scale, w, h, paint)
 }
+
+class SqRotShiftDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
