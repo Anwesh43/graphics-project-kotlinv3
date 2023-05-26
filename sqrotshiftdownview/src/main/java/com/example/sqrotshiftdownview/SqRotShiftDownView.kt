6569f471bd57d97b6eadc79ff2a0419ab6e9a77a
Anwesh43 <anweshthecool0@gmail.com>
2023-05-27 01:34:26 +0530

    sqrotshiftdownview: adding Canvas extension methods

--- sqrotshiftdownview/src/main/java/com/example/sqrotshiftdownview/SqRotShiftDownView.kt
+++ sqrotshiftdownview/src/main/java/com/example/sqrotshiftdownview/SqRotShiftDownView.kt
@@ -28,3 +28,28 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y  : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqRotShiftDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + size * dsc(2), h / 2 + (h / 2) * dsc(3)) {
+        rotate(rot * dsc(1))
+        drawRect(RectF(0f, -size * dsc(0), size, 0f), paint)
+    }
+}
+
+fun Canvas.drawSRSDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawSqRotShiftDown(scale, w, h, paint)
+}
