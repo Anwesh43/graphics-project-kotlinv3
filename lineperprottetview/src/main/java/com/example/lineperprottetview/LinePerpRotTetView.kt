10deec0cd29e928a0ab4297e11f6085704388ee3
Anwesh43 <anweshthecool0@gmail.com>
2023-05-29 15:01:09 +0530

    lineperprottetview: adding LPRTNode class

--- lineperprottetview/src/main/java/com/example/lineperprottetview/LinePerpRotTetView.kt
+++ lineperprottetview/src/main/java/com/example/lineperprottetview/LinePerpRotTetView.kt
@@ -125,4 +125,45 @@ class LinePerpRotTetView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LPRTNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LPRTNode? = null
+        private var prev : LPRTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LPRTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLPRTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LPRTNode {
+            var curr : LPRTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
