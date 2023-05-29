e7b40c10e6a231b31fb4d1853d636f63cd487251
Anwesh43 <anweshthecool0@gmail.com>
2023-05-29 14:50:44 +0530

    lineperprottetview: adding LinePerpRotTetView class

--- lineperprottetview/src/main/java/com/example/lineperprottetview/LinePerpRotTetView.kt
+++ lineperprottetview/src/main/java/com/example/lineperprottetview/LinePerpRotTetView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLPRTNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLinePerpRotTet(scale, w, h, paint)
 }
+
+class LinePerpRotTetView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
