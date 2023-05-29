18a1ff67d3b3c1f80fb78c6e1215b0354ce04ea5
Anwesh43 <anweshthecool0@gmail.com>
2023-05-29 15:08:15 +0530

    lineperprottetview: adding Renderer class

--- lineperprottetview/src/main/java/com/example/lineperprottetview/LinePerpRotTetView.kt
+++ lineperprottetview/src/main/java/com/example/lineperprottetview/LinePerpRotTetView.kt
@@ -189,4 +189,27 @@ class LinePerpRotTetView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LinePerpRotTetView) {
+
+        private val animator : Animator = Animator(view)
+        private val lprt : LinePerpRotTet = LinePerpRotTet(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lprt.draw(canvas, paint)
+            animator.animate {
+                lprt.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lprt.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
