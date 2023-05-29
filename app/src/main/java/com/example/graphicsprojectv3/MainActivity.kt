93f861a422a97578d24a5ccc189fd2373726fe18
Anwesh43 <anweshthecool0@gmail.com>
2023-05-29 15:14:44 +0530

    lineperprottetview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqrotshiftdownview.SqRotShiftDownView
+import com.example.lineperprottetview.LinePerpRotTetView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SqRotShiftDownView.create(this)
+        LinePerpRotTetView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
