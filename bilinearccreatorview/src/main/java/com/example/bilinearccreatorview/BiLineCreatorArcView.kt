1375087235445c1adc529a88399ec524411a7df8
Anwesh43 <anweshthecool0@gmail.com>
2023-05-31 05:23:26 +0530

    bilinearccreatorview: adding Canvas extension methods

--- bilinearccreatorview/src/main/java/com/example/bilinearccreatorview/BiLineCreatorArcView.kt
+++ bilinearccreatorview/src/main/java/com/example/bilinearccreatorview/BiLineCreatorArcView.kt
@@ -30,3 +30,37 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiLineCreatorArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        drawLine(0f, -size * 0.5f * dsc(1), 0f, size * 0.5f * dsc(1), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawArc(RectF(-size, -size / 2, 0f, size / 2), 0f, deg * dsc(0), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBLCANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLineCreatorArc(scale, w, h, paint)
+}
