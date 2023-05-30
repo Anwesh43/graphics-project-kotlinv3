e27168c0d066d83c0ddadae5023f207d32feb74e
Anwesh43 <anweshthecool0@gmail.com>
2023-05-31 05:17:13 +0530

    bilinearccreatorview: adding constants and libraries

---
+++ bilinearccreatorview/src/main/java/com/example/bilinearccreatorview/BiLineCreatorArcView.kt
@@ -0,0 +1,28 @@
+package com.example.bilinearccreatorview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val rot : Float = 180f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sizeFactor : Float = 4.9f
+val deg : Float = 120f
+val strokeFactor : Float = 90f
