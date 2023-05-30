f2e313f054eb77a08d08ceed4940297f3916dc05
Anwesh43 <anweshthecool0@gmail.com>
2023-05-31 05:24:37 +0530

    bilinearccreatorview: adding BiLineCreatorArcView class

--- bilinearccreatorview/src/main/java/com/example/bilinearccreatorview/BiLineCreatorArcView.kt
+++ bilinearccreatorview/src/main/java/com/example/bilinearccreatorview/BiLineCreatorArcView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawBLCANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineCreatorArc(scale, w, h, paint)
 }
+
+class BiLineCreatorArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
