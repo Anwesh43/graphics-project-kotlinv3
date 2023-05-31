dc9bdced2737cd0e06933c4105d34c4c3b9ff0c2
Anwesh43 <anweshthecool0@gmail.com>
2023-05-31 05:46:26 +0530

    bilinearccreatorview: adding Renderer class

--- bilinearccreatorview/src/main/java/com/example/bilinearccreatorview/BiLineCreatorArcView.kt
+++ bilinearccreatorview/src/main/java/com/example/bilinearccreatorview/BiLineCreatorArcView.kt
@@ -191,4 +191,27 @@ class BiLineCreatorArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiLineCreatorArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val blca : BiLineCreatorArc = BiLineCreatorArc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            blca.draw(canvas, paint)
+            animator.animate {
+                blca.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            blca.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
