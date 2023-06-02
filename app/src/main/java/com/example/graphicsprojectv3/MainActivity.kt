c75e50b13328ac900b4eb28708f2c90234e5c700
Anwesh43 <anweshthecool0@gmail.com>
2023-06-03 00:52:37 +0530

    sqarcdownrotview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.bilinearccreatorview.BiLineCreatorArcView
+import com.example.sqarcdownrotview.SqArcDownRotView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BiLineCreatorArcView.create(this)
+        SqArcDownRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
