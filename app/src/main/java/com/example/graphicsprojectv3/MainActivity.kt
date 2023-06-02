be964a0b1a667392908dd6d01d3673d7153a766b
Anwesh43 <anweshthecool0@gmail.com>
2023-06-03 00:11:46 +0530

    bilinecreatorarcview: adding module setup

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineperprottetview.LinePerpRotTetView
+import com.example.bilinearccreatorview.BiLineCreatorArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LinePerpRotTetView.create(this)
+        BiLineCreatorArcView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
