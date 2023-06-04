63d0144f39b621c8645f54c69a6b14cabe025a91
Anwesh43 <anweshthecool0@gmail.com>
2023-06-04 15:21:06 +0530

    squarelinedroprightview: adding SLDRNode class

--- squarelinedroprightview/src/main/java/com/example/squarelinedroprightview/SquareLineDropRightView.kt
+++ squarelinedroprightview/src/main/java/com/example/squarelinedroprightview/SquareLineDropRightView.kt
@@ -125,4 +125,45 @@ class SquareLineDropRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SLDRNode(var i : Int, val state : State = State()) {
+
+        private var next : SLDRNode? = null
+        private var prev : SLDRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SLDRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSLDRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SLDRNode {
+            var curr : SLDRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
