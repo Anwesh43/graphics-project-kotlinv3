63349f023618f408029fe74765e4f94fce8d3b99
Anwesh43 <anweshthecool0@gmail.com>
2023-06-04 14:21:02 +0530

    squarelinedroprightview: adding Canvas extension methods

--- squarelinedroprightview/src/main/java/com/example/squarelinedroprightview/SquareLineDropRightView.kt
+++ squarelinedroprightview/src/main/java/com/example/squarelinedroprightview/SquareLineDropRightView.kt
@@ -29,3 +29,36 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSquareLineDropRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY((w / 2) * dsc(3), 0f) {
+            rotate(rot * dsc(1))
+            drawRect(RectF(-size * dsc(0), -size, 0f, 0f), paint)
+        }
+        drawXY(0f, (h / 2) * dsc(2)) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawSLDRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareLineDropRight(scale, w, h, paint)
+}
