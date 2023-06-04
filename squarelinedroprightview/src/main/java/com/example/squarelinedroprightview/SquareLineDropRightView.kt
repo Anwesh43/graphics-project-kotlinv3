ba12d54f79c84658ecc06d948259cd976064a7f1
Anwesh43 <anweshthecool0@gmail.com>
2023-06-04 14:24:12 +0530

    squarelinedroprightview: adding SquareLineDropRightView class

--- squarelinedroprightview/src/main/java/com/example/squarelinedroprightview/SquareLineDropRightView.kt
+++ squarelinedroprightview/src/main/java/com/example/squarelinedroprightview/SquareLineDropRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawSLDRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSquareLineDropRight(scale, w, h, paint)
 }
+
+class SquareLineDropRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
