aad98e7e4c32c7a5e44cd0f9792aa5c98905ccfd
Anwesh43 <anweshthecool0@gmail.com>
2023-06-05 09:40:12 +0530

    linesqrotrightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.squarelinedroprightview.SquareLineDropRightView
+import com.example.linesqrotrightview.LineSqRotRightView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SquareLineDropRightView.create(this)
+        LineSqRotRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
