273999e988c05f87cb9a6c57e40b522091c3d50d
Anwesh43 <anweshthecool0@gmail.com>
2023-06-05 09:39:44 +0530

    linesqrotrightview: adding static create method

--- linesqrotrightview/src/main/java/com/example/linesqrotrightview/LineSqRotRightView.kt
+++ linesqrotrightview/src/main/java/com/example/linesqrotrightview/LineSqRotRightView.kt
@@ -211,4 +211,13 @@ class LineSqRotRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineSqRotRightView {
+            val view : LineSqRotRightView = LineSqRotRightView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
