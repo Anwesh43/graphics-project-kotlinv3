2122cabde97fd29aab37ebc91a2255c80803faf4
Anwesh43 <anweshthecool0@gmail.com>
2023-06-06 11:38:23 +0530

    blocklinerightview: adding BlockLineRightView class

--- blocklinerightview/src/main/java/com/example/blocklinerightview/BlockLineRightView.kt
+++ blocklinerightview/src/main/java/com/example/blocklinerightview/BlockLineRightView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBlockLineRight(scale, w, h, paint)
 }
+
+class BlockLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
