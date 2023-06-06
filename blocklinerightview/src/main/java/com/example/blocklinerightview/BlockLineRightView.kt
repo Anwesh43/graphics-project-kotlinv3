cacc438d3d348a2030e0644b18f583e84e4fe1d9
Anwesh43 <anweshthecool0@gmail.com>
2023-06-06 11:42:36 +0530

    blocklinerightview: adding BLRNode class

--- blocklinerightview/src/main/java/com/example/blocklinerightview/BlockLineRightView.kt
+++ blocklinerightview/src/main/java/com/example/blocklinerightview/BlockLineRightView.kt
@@ -124,4 +124,45 @@ class BlockLineRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLRNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : BLRNode? = null
+        private var next : BLRNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLRNode {
+            var curr : BLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
