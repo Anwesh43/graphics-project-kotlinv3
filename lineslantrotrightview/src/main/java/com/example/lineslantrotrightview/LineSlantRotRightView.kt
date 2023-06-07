04ad989745e802f8a76b3176d93ae09650020686
Anwesh43 <anweshthecool0@gmail.com>
2023-06-07 07:50:55 +0530

    lineslantrottightview: adding LineSlantRotRightView class

--- lineslantrotrightview/src/main/java/com/example/lineslantrotrightview/LineSlantRotRightView.kt
+++ lineslantrotrightview/src/main/java/com/example/lineslantrotrightview/LineSlantRotRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLSRRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSlantRotRight(scale, w, h, paint)
 }
+
+class LineSlantRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
