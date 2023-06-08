01dadff5af61f8f7e3f728da6d7b57bf83c62f34
Anwesh43 <anweshthecool0@gmail.com>
2023-06-08 15:06:07 +0530

    lineslantrottightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.blocklinerightview.BlockLineRightView
+import com.example.lineslantrotrightview.LineSlantRotRightView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BlockLineRightView.create(this)
+        LineSlantRotRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
