18b5ac25a6a720ea97ea346567d8ae670e60f7b4
Anwesh43 <anweshthecool0@gmail.com>
2023-06-12 19:23:49 +0530

    lineminisweeprightview: adding LineMiniSweepRightView class

--- lineminisweeprightview/src/main/java/com/example/lineminisweeprightview/LineMiniSweepRightView.kt
+++ lineminisweeprightview/src/main/java/com/example/lineminisweeprightview/LineMiniSweepRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLMSRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineMiniSweepRight(scale, w, h, paint)
 }
+
+class LineMiniSweepRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
