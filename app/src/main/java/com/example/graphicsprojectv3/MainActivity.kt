c310e1a2d9303229f28b6f4f3663f4984f2df1aa
Anwesh43 <anweshthecool0@gmail.com>
2023-06-13 09:20:31 +0530

    lineminisweeprightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineslantrotrightview.LineSlantRotRightView
+import com.example.lineminisweeprightview.LineMiniSweepRightView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineSlantRotRightView.create(this)
+        LineMiniSweepRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
