e2633978c025e0a9997d0cf6b535d5729c104599
Anwesh43 <anweshthecool0@gmail.com>
2023-06-14 12:56:36 +0530

    midlinerotupview: adding Canvas extension  methods

--- midlinerotupview/src/main/java/com/example/midlinerotupview/MidLineRotUpView.kt
+++ midlinerotupview/src/main/java/com/example/midlinerotupview/MidLineRotUpView.kt
@@ -24,7 +24,44 @@ val sizeFactor : Float = 4.9f
 val rot : Float = 90f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawMidLineRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY(0f, -h / 2 + (h / 2) * dsc(1)) {
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(deg * dsc(1))
+                drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawMLUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMidLineRotUp(scale, w, h, paint)
+}
