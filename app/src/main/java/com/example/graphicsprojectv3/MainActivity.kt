6aeade44de3a47bd370fd3dc5934754da6408c2d
Anwesh43 <anweshthecool0@gmail.com>
2023-06-14 13:22:33 +0530

    midlinerotupview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineminisweeprightview.LineMiniSweepRightView
+import com.example.midlinerotupview.MidLineRotUpView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineMiniSweepRightView.create(this)
+        MidLineRotUpView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
