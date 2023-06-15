338f43351a2d719a5a89ec435687055f08a64628
Anwesh43 <anweshthecool0@gmail.com>
2023-06-15 10:39:21 +0530

    sqlineslantrightview: adding Canvas extension methods

--- sqlineslantrightview/src/main/java/com/example/sqlineslantrightview/SqLineSlantRightView.kt
+++ sqlineslantrightview/src/main/java/com/example/sqlineslantrightview/SqLineSlantRightView.kt
@@ -24,9 +24,38 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqLineSlantRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawRect(RectF(0f, -size / 2, size * dsc(0), size / 2), paint)
+        drawXY(size, size / 2) {
+            rotate(45f * (1 - dsc(2)))
+            drawLine(0f, 0f, 0f, size * dsc(1), paint)
+        }
+    }
+}
+
+fun Canvas.drawSLSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSqLineSlantRight(scale, w, h, paint)
+}
