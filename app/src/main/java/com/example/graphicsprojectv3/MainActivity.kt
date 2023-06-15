aecdab111d02453df1049a093fbecf8040fbd507
Anwesh43 <anweshthecool0@gmail.com>
2023-06-15 11:35:39 +0530

    sqlineslantrightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.midlinerotupview.MidLineRotUpView
+import com.example.sqlineslantrightview.SqLineSlantRightView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        MidLineRotUpView.create(this)
+        SqLineSlantRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
