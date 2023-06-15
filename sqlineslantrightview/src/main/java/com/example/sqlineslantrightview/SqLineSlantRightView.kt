be6d9af9aae48006681889cb14daadc7e5a7a487
Anwesh43 <anweshthecool0@gmail.com>
2023-06-15 11:25:48 +0530

    sqlineslantrightview: adding SLSRNode class

--- sqlineslantrightview/src/main/java/com/example/sqlineslantrightview/SqLineSlantRightView.kt
+++ sqlineslantrightview/src/main/java/com/example/sqlineslantrightview/SqLineSlantRightView.kt
@@ -122,4 +122,45 @@ class SqLineSlantRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SLSRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SLSRNode? = null
+        private var prev : SLSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SLSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSLSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SLSRNode {
+            var curr : SLSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
