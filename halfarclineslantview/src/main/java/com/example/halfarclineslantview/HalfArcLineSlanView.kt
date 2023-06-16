8a2e40ff8166ae3431470f250fc6d77f3484c8e3
Anwesh43 <anweshthecool0@gmail.com>
2023-06-16 08:11:47 +0530

    halfarclineslantview: adding HalfArcLineSlantView class

--- halfarclineslantview/src/main/java/com/example/halfarclineslantview/HalfArcLineSlanView.kt
+++ halfarclineslantview/src/main/java/com/example/halfarclineslantview/HalfArcLineSlanView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawHALSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHalfArcLineSlant(scale, w, h, paint)
 }
+
+class HalfArcLineSlantView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
