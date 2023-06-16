bf238553aa50b8aa612619e519adddbbd6a5787a
Anwesh43 <anweshthecool0@gmail.com>
2023-06-16 08:10:44 +0530

    halfarclineslantview: adding Canvas extension methods

--- halfarclineslantview/src/main/java/com/example/halfarclineslantview/HalfArcLineSlanView.kt
+++ halfarclineslantview/src/main/java/com/example/halfarclineslantview/HalfArcLineSlanView.kt
@@ -30,3 +30,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfArcLineSlant(scale : Float, w : Float, h : Float, paint: Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size / 2) * dsc(3), h / 2) {
+        drawXY(0f, -size / 2) {
+            rotate(-rot * (1 - dsc(2)))
+            drawLine(0f, 0f, size * 0.5f * dsc(1), 0f, paint)
+        }
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 90f, 180f * dsc(0), false, paint)
+    }
+}
+
+fun Canvas.drawHALSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfArcLineSlant(scale, w, h, paint)
+}
