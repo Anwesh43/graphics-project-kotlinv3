7dad442931863020d498d6e00b91c79e11951fec
Anwesh43 <anweshthecool0@gmail.com>
2023-06-16 08:42:30 +0530

    halfarclineslantview: adding HALSNode class

--- halfarclineslantview/src/main/java/com/example/halfarclineslantview/HalfArcLineSlanView.kt
+++ halfarclineslantview/src/main/java/com/example/halfarclineslantview/HalfArcLineSlanView.kt
@@ -122,4 +122,45 @@ class HalfArcLineSlantView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HALSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HALSNode? = null
+        private var prev : HALSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HALSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHALSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HALSNode {
+            var curr : HALSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
