c9f33f96d999c91a06ffecf2329e9c6126cd8302
Anwesh43 <anweshthecool0@gmail.com>
2023-06-17 11:13:57 +0530

    linefromleftrotview: adding view in acitivty

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.halfarclineslantview.HalfArcLineSlantView
+import com.example.linefromleftrotview.LineFromLeftRotView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        HalfArcLineSlantView.create(this)
+        LineFromLeftRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
