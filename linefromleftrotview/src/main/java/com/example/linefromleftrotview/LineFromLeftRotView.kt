449f6c4e3ed0a80f2dc8ba35ac023c91d20b2c89
Anwesh43 <anweshthecool0@gmail.com>
2023-06-17 11:09:38 +0530

    linefromleftrotview: adding Renderer class

--- linefromleftrotview/src/main/java/com/example/linefromleftrotview/LineFromLeftRotView.kt
+++ linefromleftrotview/src/main/java/com/example/linefromleftrotview/LineFromLeftRotView.kt
@@ -186,4 +186,27 @@ class LineFromLeftRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineFromLeftRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val lflr : LineFromLeftRot = LineFromLeftRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lflr.draw(canvas, paint)
+            animator.animate {
+                lflr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lflr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
