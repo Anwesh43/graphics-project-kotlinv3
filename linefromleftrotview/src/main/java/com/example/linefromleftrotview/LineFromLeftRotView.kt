3a9a226ff5ca3a615341a9a7083680a464881a4b
Anwesh43 <anweshthecool0@gmail.com>
2023-06-17 11:02:25 +0530

    linefromleftrotview: adding LFLRNode class

--- linefromleftrotview/src/main/java/com/example/linefromleftrotview/LineFromLeftRotView.kt
+++ linefromleftrotview/src/main/java/com/example/linefromleftrotview/LineFromLeftRotView.kt
@@ -122,4 +122,45 @@ class LineFromLeftRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LFLRNode(var i : Int, val state : State = State()) {
+
+        private var next : LFLRNode? = null
+        private var prev : LFLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LFLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLFLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LFLRNode {
+            var curr : LFLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
