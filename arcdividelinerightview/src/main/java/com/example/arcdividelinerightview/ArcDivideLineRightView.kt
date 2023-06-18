58b4d895a553b4add74c8211c49114900dab7736
Anwesh43 <anweshthecool0@gmail.com>
2023-06-18 12:08:16 +0530

    arcdividelinerightview: adding ArcDivideLineRightView class

--- arcdividelinerightview/src/main/java/com/example/arcdividelinerightview/ArcDivideLineRightView.kt
+++ arcdividelinerightview/src/main/java/com/example/arcdividelinerightview/ArcDivideLineRightView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawADLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcDivideLineRight(scale, w, h, paint)
 }
+
+class ArcDivideLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
