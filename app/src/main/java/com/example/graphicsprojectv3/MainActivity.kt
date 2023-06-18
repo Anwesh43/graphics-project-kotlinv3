01e5fc71c26116fff0ebd6d0c30d9081a11d4cef
Anwesh43 <anweshthecool0@gmail.com>
2023-06-18 12:30:17 +0530

    arcdividelinerightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linefromleftrotview.LineFromLeftRotView
+import com.example.arcdividelinerightview.ArcDivideLineRightView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineFromLeftRotView.create(this)
+        ArcDivideLineRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
