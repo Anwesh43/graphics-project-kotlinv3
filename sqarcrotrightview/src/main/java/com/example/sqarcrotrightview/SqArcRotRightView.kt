d92834a6732d9bd4cd937f34db7dfd57981d6322
Anwesh43 <anweshthecool0@gmail.com>
2023-06-19 10:46:17 +0530

    sqarcrotrightview: adding Canvas extension methods

--- sqarcrotrightview/src/main/java/com/example/sqarcrotrightview/SqArcRotRightView.kt
+++ sqarcrotrightview/src/main/java/com/example/sqarcrotrightview/SqArcRotRightView.kt
@@ -29,3 +29,38 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqArcRotRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2 + (w / 2 + size / 2) * dsc(3), h / 2) {
+        drawRect(RectF(0f, -size / 2, size * dsc(0), size / 2), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                drawXY(0f, size / 2) {
+                    rotate(rot * dsc(2))
+                    drawArc(RectF(-size / 2, -size / 4, 0f, size / 4), 0f, 180f * dsc(1), true, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawSARRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSqArcRotRight(scale, w, h, paint)
+}
