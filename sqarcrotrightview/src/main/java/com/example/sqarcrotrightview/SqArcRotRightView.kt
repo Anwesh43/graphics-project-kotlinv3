ebaec48764bb5ff9d0473dc593f8986daf1e942f
Anwesh43 <anweshthecool0@gmail.com>
2023-06-19 10:56:06 +0530

    sqarcrotrightview: adding SqArcRotRightView class

--- sqarcrotrightview/src/main/java/com/example/sqarcrotrightview/SqArcRotRightView.kt
+++ sqarcrotrightview/src/main/java/com/example/sqarcrotrightview/SqArcRotRightView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawSARRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSqArcRotRight(scale, w, h, paint)
 }
+
+class SqArcRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
