a09e3b4f4a00bf580c4cdbb7b7ded2133ecc27cc
Anwesh43 <anweshthecool0@gmail.com>
2023-06-19 11:55:31 +0530

    sqarcrotrightview: adding view in acitivty

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqarcdownrotview.SqArcDownRotView
+import com.example.sqarcrotrightview.SqArcRotRightView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SqArcDownRotView.create(this)
+        SqArcRotRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
