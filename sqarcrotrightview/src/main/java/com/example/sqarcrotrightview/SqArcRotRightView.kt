20cbd79192abfa5a5cb5d6da06eef7408294b248
Anwesh43 <anweshthecool0@gmail.com>
2023-06-19 11:44:19 +0530

    sqarcrotrightview: adding Renderer class

--- sqarcrotrightview/src/main/java/com/example/sqarcrotrightview/SqArcRotRightView.kt
+++ sqarcrotrightview/src/main/java/com/example/sqarcrotrightview/SqArcRotRightView.kt
@@ -191,4 +191,27 @@ class SqArcRotRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SqArcRotRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val sarr : SqArcRotRight = SqArcRotRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sarr.draw(canvas, paint)
+            animator.animate {
+                sarr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sarr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
