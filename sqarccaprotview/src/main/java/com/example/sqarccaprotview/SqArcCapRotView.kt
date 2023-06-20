7baf169c02f7f3af04152fb0b20dc1f4c81a4bae
Anwesh43 <anweshthecool0@gmail.com>
2023-06-20 14:33:55 +0530

    sqarccaprotview: adding SACRNode class

--- sqarccaprotview/src/main/java/com/example/sqarccaprotview/SqArcCapRotView.kt
+++ sqarccaprotview/src/main/java/com/example/sqarccaprotview/SqArcCapRotView.kt
@@ -123,4 +123,45 @@ class SqArcCapRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SACRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SACRNode? = null
+        private var prev : SACRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SACRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSACRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SACRNode {
+            var curr : SACRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
