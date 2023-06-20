94dfb47c0d4e36107036fdb4eb1fc8f5c54a7ed8
Anwesh43 <anweshthecool0@gmail.com>
2023-06-21 03:05:47 +0530

    linecornersqdownview: adding Renderer class

--- linecornersqdownview/src/main/java/com/example/linecornersqdownview/LineCornerSqDownView.kt
+++ linecornersqdownview/src/main/java/com/example/linecornersqdownview/LineCornerSqDownView.kt
@@ -191,4 +191,27 @@ class LineCornerSqDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineCornerSqDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val lcsd : LineCornerSqDown = LineCornerSqDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lcsd.draw(canvas, paint)
+            animator.animate {
+                lcsd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lcsd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
