09118288d30bed10fc4ba8be073eef53a3ec328d
Anwesh43 <anweshthecool0@gmail.com>
2023-06-20 15:14:16 +0530

    sqarccaprotview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqarcrotrightview.SqArcRotRightView
+import com.example.sqarccaprotview.SqArcCapRotView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SqArcRotRightView.create(this)
+        SqArcCapRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
