760bc4d6e477ca1898bbdc9234080650f85b1646
Anwesh43 <anweshthecool0@gmail.com>
2023-06-21 03:07:31 +0530

    linecornersqdownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqarccaprotview.SqArcCapRotView
+import com.example.linecornersqdownview.LineCornerSqDownView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SqArcCapRotView.create(this)
+        LineCornerSqDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
