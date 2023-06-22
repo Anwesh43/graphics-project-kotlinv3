bfea59e9f3ae3be6fbf36423ae5051f52404b305
Anwesh43 <anweshthecool0@gmail.com>
2023-06-22 11:34:35 +0530

    linesqdownrotleftview: adding constants and libraries

---
+++ linesqdownrotleftview/src/main/java/com/example/linesqdownrotleftview/LineSqDownLeftView.kt
@@ -0,0 +1,27 @@
+package com.example.linesqdownrotleftview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor("#BDBDBD")
+}.toTypedArray()
+val sizeFactor : Float = 4.9f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 180f
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
