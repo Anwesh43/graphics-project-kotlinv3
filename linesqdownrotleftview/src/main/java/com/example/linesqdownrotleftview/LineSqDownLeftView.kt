3a1c909602b08a2f4d1b879aa17b6b0ebc4178a4
Anwesh43 <anweshthecool0@gmail.com>
2023-06-22 11:56:28 +0530

    linesqdownrotleftview: adding LineSqDownLeftView class

--- linesqdownrotleftview/src/main/java/com/example/linesqdownrotleftview/LineSqDownLeftView.kt
+++ linesqdownrotleftview/src/main/java/com/example/linesqdownrotleftview/LineSqDownLeftView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLSDLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSqDownLeft(scale, w, h, paint)
 }
+
+class LineSqDownLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
