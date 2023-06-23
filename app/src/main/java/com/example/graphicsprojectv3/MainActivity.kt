856d008e80ff503e12fe248a6bfed6a3ed864ca0
Anwesh43 <anweshthecool0@gmail.com>
2023-06-23 11:12:53 +0530

    linebrealtoonerightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linesqdownrotleftview.LineSqDownLeftView
+import com.example.linebreaktoonerightview.LineBreakToOneRightView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineSqDownLeftView.create(this)
+        LineBreakToOneRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
