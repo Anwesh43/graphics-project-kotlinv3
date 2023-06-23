744effc738fe7898fad898edef32d9aa15c1c85f
Anwesh43 <anweshthecool0@gmail.com>
2023-06-23 10:58:29 +0530

    linebrealtoonerightview: adding LBTONode class

--- linebreaktoonerightview/src/main/java/com/example/linebreaktoonerightview/LineBreakToOneRightView.kt
+++ linebreaktoonerightview/src/main/java/com/example/linebreaktoonerightview/LineBreakToOneRightView.kt
@@ -123,4 +123,45 @@ class LineBreakToOneRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBTONode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBTONode? = null
+        private var prev : LBTONode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBTONode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBTONode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBTONode {
+            var curr : LBTONode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
