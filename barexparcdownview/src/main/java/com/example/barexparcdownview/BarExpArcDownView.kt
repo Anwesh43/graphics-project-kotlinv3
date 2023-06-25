3440f047cf0bced440573400ebe71013aacf0648
Anwesh43 <anweshthecool0@gmail.com>
2023-06-25 12:25:25 +0530

    barexparcdownview: adding Canvas extension methods

--- barexparcdownview/src/main/java/com/example/barexparcdownview/BarExpArcDownView.kt
+++ barexparcdownview/src/main/java/com/example/barexparcdownview/BarExpArcDownView.kt
@@ -29,3 +29,31 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarExpArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size / 2) * dsc(3)) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawRect(RectF(-size * dsc(1), 0f, 0f, size * 0.5f), paint)
+        }
+        drawArc(RectF(0f, -size / 2, size, size / 2), 0f, 180f * dsc(0), true, paint)
+    }
+}
+
+fun Canvas.drawBEADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBarExpArcDown(scale, w, h, paint)
+}
