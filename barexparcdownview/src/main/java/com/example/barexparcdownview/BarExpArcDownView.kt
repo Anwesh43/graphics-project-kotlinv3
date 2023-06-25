4d88a016bdfa855aa091f9ff19304f3066862c94
Anwesh43 <anweshthecool0@gmail.com>
2023-06-25 12:34:01 +0530

    barexparcdownview: adding BEADNode class

--- barexparcdownview/src/main/java/com/example/barexparcdownview/BarExpArcDownView.kt
+++ barexparcdownview/src/main/java/com/example/barexparcdownview/BarExpArcDownView.kt
@@ -120,4 +120,45 @@ class BarExpArcDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BEADNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BEADNode? = null
+        private var prev : BEADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BEADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBEADNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BEADNode {
+            var curr : BEADNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
