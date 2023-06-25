70fa9b216d4071a6dca4b06218ce55ce738c3e55
Anwesh43 <anweshthecool0@gmail.com>
2023-06-25 12:43:08 +0530

    barexparcdownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linearccompleteupview.LineArcCompleteUpView
+import com.example.barexparcdownview.BarExpArcDownView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineArcCompleteUpView.create(this)
+        BarExpArcDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
