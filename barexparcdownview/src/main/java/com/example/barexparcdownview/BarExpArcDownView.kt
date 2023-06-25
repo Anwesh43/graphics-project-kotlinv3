b8aedf1f9bc7d85129b7d91c9eacd566ad467955
Anwesh43 <anweshthecool0@gmail.com>
2023-06-25 12:39:27 +0530

    barexparcdownview: adding Renderer class

--- barexparcdownview/src/main/java/com/example/barexparcdownview/BarExpArcDownView.kt
+++ barexparcdownview/src/main/java/com/example/barexparcdownview/BarExpArcDownView.kt
@@ -184,4 +184,27 @@ class BarExpArcDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BarExpArcDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val bead : BarExpArcDown = BarExpArcDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bead.draw(canvas, paint)
+            animator.animate {
+                bead.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bead.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
