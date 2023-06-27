3a7c08e3d0e263933fb90684b981557d190aa2b3
Anwesh43 <anweshthecool0@gmail.com>
2023-06-27 10:55:09 +0530

    linearcsqupview: adding LASUNode class

--- linearcsqupview/src/main/java/com/example/linearcsqupview/LineArcSqUpView.kt
+++ linearcsqupview/src/main/java/com/example/linearcsqupview/LineArcSqUpView.kt
@@ -129,4 +129,45 @@ class LineArcSqUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LASUNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LASUNode? = null
+        private var next : LASUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LASUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLASUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LASUNode {
+            var curr : LASUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
