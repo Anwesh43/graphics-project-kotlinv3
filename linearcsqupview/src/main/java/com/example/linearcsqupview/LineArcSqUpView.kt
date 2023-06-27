8a77b4951f5685173569babf8a2a66aa16219b37
Anwesh43 <anweshthecool0@gmail.com>
2023-06-27 10:41:22 +0530

    linearcsqupview: adding Canvas extension methods

--- linearcsqupview/src/main/java/com/example/linearcsqupview/LineArcSqUpView.kt
+++ linearcsqupview/src/main/java/com/example/linearcsqupview/LineArcSqUpView.kt
@@ -29,3 +29,41 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcSqUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, -h / 2 + (h / 2) * dsc(0)) {
+                rotate(rot * dsc(1))
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+            paint.style = Paint.Style.STROKE
+            drawArc(RectF(-size, -size, size, size), -90f, rot * dsc(1), false, paint)
+            paint.style = Paint.Style.FILL
+            drawXY(0f, h / 2 - (h / 2) * dsc(2)) {
+                drawRect(RectF(0f, 0f, size, size), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLASUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcSqUp(scale, w, h, paint)
+}
+
