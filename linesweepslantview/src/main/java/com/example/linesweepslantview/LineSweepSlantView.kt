1ad4a747e3b47ef05064df2d93f6a7b99b738f84
Anwesh43 <anweshthecool0@gmail.com>
2023-06-28 09:28:06 +0530

    linesweepslantview: adding Canvas extension methods

--- linesweepslantview/src/main/java/com/example/linesweepslantview/LineSweepSlantView.kt
+++ linesweepslantview/src/main/java/com/example/linesweepslantview/LineSweepSlantView.kt
@@ -17,14 +17,46 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor("#BDBDBD")
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val rot : Float = 90f
+val deg : Float = 45f
 val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineSweepSlant(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * j * dsc(1) + deg * dsc(j + 2))
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLSSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSweepSlant(scale, w, h, paint)
+}
