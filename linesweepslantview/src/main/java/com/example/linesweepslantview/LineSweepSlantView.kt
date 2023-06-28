ddd7c09aeb734fb3544ac7c32ea10522675c1154
Anwesh43 <anweshthecool0@gmail.com>
2023-06-28 09:29:57 +0530

    linesweepslantview: adding LineSweepSlantView class

--- linesweepslantview/src/main/java/com/example/linesweepslantview/LineSweepSlantView.kt
+++ linesweepslantview/src/main/java/com/example/linesweepslantview/LineSweepSlantView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLSSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSweepSlant(scale, w, h, paint)
 }
+
+class LineSweepSlantView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
