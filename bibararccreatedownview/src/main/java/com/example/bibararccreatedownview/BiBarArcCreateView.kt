db9d8119552b5297db1460896bf23c48630a4871
Anwesh43 <anweshthecool0@gmail.com>
2023-06-29 11:12:35 +0530

    bibarccreatedownview: adding BBACNode class

--- bibararccreatedownview/src/main/java/com/example/bibararccreatedownview/BiBarArcCreateView.kt
+++ bibararccreatedownview/src/main/java/com/example/bibararccreatedownview/BiBarArcCreateView.kt
@@ -123,4 +123,45 @@ class BiBarArcCreateView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BBACNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : BBACNode? = null
+        private var next : BBACNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BBACNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBACNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBACNode {
+            var curr : BBACNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
