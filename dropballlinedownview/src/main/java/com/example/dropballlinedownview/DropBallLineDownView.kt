7070dd2fb518d5e703378c91e59caf85c8dbe757
Anwesh43 <anweshthecool0@gmail.com>
2023-06-30 07:26:35 +0530

    dropballlinedownview: adding DBLDNode class

--- dropballlinedownview/src/main/java/com/example/dropballlinedownview/DropBallLineDownView.kt
+++ dropballlinedownview/src/main/java/com/example/dropballlinedownview/DropBallLineDownView.kt
@@ -126,4 +126,45 @@ class DropBallLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DBLDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : DBLDNode? = null
+        private var prev : DBLDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DBLDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDBLDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DBLDNode {
+            var curr : DBLDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
