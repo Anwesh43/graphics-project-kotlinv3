56d05130b6fc73a8d3e5ed8aebccde9cb8902978
Anwesh43 <anweshthecool0@gmail.com>
2023-06-30 07:10:13 +0530

    dropballlinedownview: adding DropBallLineDownView class

--- dropballlinedownview/src/main/java/com/example/dropballlinedownview/DropBallLineDownView.kt
+++ dropballlinedownview/src/main/java/com/example/dropballlinedownview/DropBallLineDownView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawDBLDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawDropBallLineDown(scale, w, h, paint)
 }
+
+class DropBallLineDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
