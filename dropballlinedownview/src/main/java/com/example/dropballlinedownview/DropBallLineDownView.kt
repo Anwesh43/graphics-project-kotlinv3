7b8abcec4d8639d874df849c93c90b74b74dc4fb
Anwesh43 <anweshthecool0@gmail.com>
2023-06-30 07:40:38 +0530

    dropballlinedownview: adding static create method

--- dropballlinedownview/src/main/java/com/example/dropballlinedownview/DropBallLineDownView.kt
+++ dropballlinedownview/src/main/java/com/example/dropballlinedownview/DropBallLineDownView.kt
@@ -215,4 +215,12 @@ class DropBallLineDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : DropBallLineDownView {
+            val view : DropBallLineDownView = DropBallLineDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
