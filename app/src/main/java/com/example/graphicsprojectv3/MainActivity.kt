6a23e55a7f719d684d547d1f8cd3e693c1bd391f
Anwesh43 <anweshthecool0@gmail.com>
2023-07-01 16:27:03 +0530

    bilinearcrotdownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.dropballlinedownview.DropBallLineDownView
+import com.example.bilinearcrotdownview.BiLineArcRotDownView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        DropBallLineDownView.create(this)
+        BiLineArcRotDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
