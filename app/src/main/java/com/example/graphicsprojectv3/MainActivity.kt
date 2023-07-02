bb28e10600a4ccf870f380f097cb1bf25906f917
Anwesh43 <anweshthecool0@gmail.com>
2023-07-02 10:56:41 +0530

    rotslidesqlineview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.bilinearcrotdownview.BiLineArcRotDownView
+import com.example.rotslidesqlineview.RotSlideSqLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BiLineArcRotDownView.create(this)
+        RotSlideSqLineView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
