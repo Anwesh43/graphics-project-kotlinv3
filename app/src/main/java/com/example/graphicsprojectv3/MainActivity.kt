3b292d95c31aededf4c0598dae797dbe5e810e3d
Anwesh43 <anweshthecool0@gmail.com>
2023-07-03 02:19:08 +0530

    sqlineparallerotview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.rotslidesqlineview.RotSlideSqLineView
+import com.example.sqlineparallelrotview.SqParallelRotLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        RotSlideSqLineView.create(this)
+        SqParallelRotLineView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
