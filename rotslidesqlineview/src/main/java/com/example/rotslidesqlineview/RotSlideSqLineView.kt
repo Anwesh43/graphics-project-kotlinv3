0320b144cc691c453021f1be69d695137bd4f266
Anwesh43 <anweshthecool0@gmail.com>
2023-07-02 10:46:48 +0530

    rotslidesqlineview: adding RSSLNode class

--- rotslidesqlineview/src/main/java/com/example/rotslidesqlineview/RotSlideSqLineView.kt
+++ rotslidesqlineview/src/main/java/com/example/rotslidesqlineview/RotSlideSqLineView.kt
@@ -126,4 +126,45 @@ class RotSlideSqLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RSSLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RSSLNode? = null
+        private var prev : RSSLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RSSLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRSSLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RSSLNode {
+            var curr : RSSLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
