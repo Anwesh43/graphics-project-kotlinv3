7e87a9b2333b31e5249fb385db78df78cae70296
Anwesh43 <anweshthecool0@gmail.com>
2023-07-02 10:36:56 +0530

    rotslidesqlineview: adding Canvas extension methods

--- rotslidesqlineview/src/main/java/com/example/rotslidesqlineview/RotSlideSqLineView.kt
+++ rotslidesqlineview/src/main/java/com/example/rotslidesqlineview/RotSlideSqLineView.kt
@@ -30,3 +30,36 @@ val rFactor : Float = 2.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRotSlideSqLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = Math.min(w, h) / rFactor
+    drawXY(w / 2, h / 2 - (h / 2 + r + size) * dsc(4)) {
+        drawXY(w / 2 - (w / 2) * dsc(1), 0f) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+        drawXY(r * dsc(3), 0f) {
+            drawArc(RectF(-r, -r, r, r), 0f, 360f * dsc(0), true, paint)
+        }
+    }
+}
+
+fun Canvas.drawRSSLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRotSlideSqLine(scale, w, h, paint)
+}
