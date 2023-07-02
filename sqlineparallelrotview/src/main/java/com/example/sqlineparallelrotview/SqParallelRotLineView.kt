06903466eeca740b340a30009c7f19d0436f227b
Anwesh43 <anweshthecool0@gmail.com>
2023-07-03 02:14:34 +0530

    sqlineparallerotview: adding SPRLNode class

--- sqlineparallelrotview/src/main/java/com/example/sqlineparallelrotview/SqParallelRotLineView.kt
+++ sqlineparallelrotview/src/main/java/com/example/sqlineparallelrotview/SqParallelRotLineView.kt
@@ -127,4 +127,45 @@ class SqParallelRotLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SPRLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SPRLNode? = null
+        private var prev : SPRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SPRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSPRLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SPRLNode {
+            var curr : SPRLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
