ae4a724c185a6f363f188d7a9e6ea823206f4435
Anwesh43 <anweshthecool0@gmail.com>
2023-07-04 10:31:26 +0530

    lineballspreadrotview: adding Canvas extension methods

--- lineballspreadrotview/src/main/java/com/example/lineballspreadrotview/LineBallSpreadRotView.kt
+++ lineballspreadrotview/src/main/java/com/example/lineballspreadrotview/LineBallSpreadRotView.kt
@@ -30,3 +30,38 @@ val rFactor : Float = 12.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    restore()
+}
+
+fun Canvas.drawLineBallSpreadRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = Math.min(w, h) / rFactor
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        rotate(deg * dsc(2))
+        drawXY(0f, -(h / 2 + r) * (1 - dsc(0))) {
+            drawCircle(0f, 0f, r, paint)
+            for (j in 0..1) {
+                drawXY(0f, 0f) {
+                    rotate(rot * (1f - 2 * j) * dsc(1))
+                    drawLine(0f, 0f, 0f, size, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBallSpreadRot(scale, w, h, paint)
+}
