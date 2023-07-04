5f30c0513cb4ecafbcba027e142438c6012ba053
Anwesh43 <anweshthecool0@gmail.com>
2023-07-04 10:32:36 +0530

    lineballspreadrotview: adding LineBallSpreadRotView class

--- lineballspreadrotview/src/main/java/com/example/lineballspreadrotview/LineBallSpreadRotView.kt
+++ lineballspreadrotview/src/main/java/com/example/lineballspreadrotview/LineBallSpreadRotView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBallSpreadRot(scale, w, h, paint)
 }
+
+class LineBallSpreadRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
