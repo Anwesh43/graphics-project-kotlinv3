4a186bb400db3361918abe001075205ba3f1877b
Anwesh43 <anweshthecool0@gmail.com>
2023-07-05 03:25:12 +0530

    balldroplinerotview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineballspreadrotview.LineBallSpreadRotView
+import com.example.balldroplinerotview.BallDropLineRotView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineBallSpreadRotView.create(this)
+        BallDropLineRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
