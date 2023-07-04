0750858c60e1072f8cbfb534cdb546526dd375a6
Anwesh43 <anweshthecool0@gmail.com>
2023-07-05 03:23:17 +0530

    balldroplinerotview: adding Renderer class

--- balldroplinerotview/src/main/java/com/example/balldroplinerotview/BallDropLineRotView.kt
+++ balldroplinerotview/src/main/java/com/example/balldroplinerotview/BallDropLineRotView.kt
@@ -190,4 +190,27 @@ class BallDropLineRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BallDropLineRotView) {
+
+        private val bdlr : BallDropLineRot = BallDropLineRot(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bdlr.draw(canvas, paint)
+            animator.animate {
+                bdlr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bdlr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
