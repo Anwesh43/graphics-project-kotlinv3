04f8615ecb8a773220d386e23b3ddddcefb9e137
Anwesh43 <anweshthecool0@gmail.com>
2023-07-05 03:14:48 +0530

    balldroplinerotview: adding Canvas extension methods

--- balldroplinerotview/src/main/java/com/example/balldroplinerotview/BallDropLineRotView.kt
+++ balldroplinerotview/src/main/java/com/example/balldroplinerotview/BallDropLineRotView.kt
@@ -28,3 +28,38 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBallDropLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = size / 4
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY((w / 2) * (1 - j) * dsc(3), (h / 2) * j * dsc(3)) {
+                rotate(rot * j * dsc(2) * (1f - 2 * j))
+                drawXY(0f, -r - h / 2 + (h / 2 - size + r) * dsc(1)) {
+                    drawCircle(0f, 0f, r, paint)
+                }
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBDLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBallDropLineRot(scale, w, h, paint)
+}
