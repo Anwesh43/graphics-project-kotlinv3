279a7b9d40fcaf7cbc5ad68a3648778db75f915f
Anwesh43 <anweshthecool0@gmail.com>
2023-07-04 10:49:16 +0530

    lineballspreadrotview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqlineparallelrotview.SqParallelRotLineView
+import com.example.lineballspreadrotview.LineBallSpreadRotView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SqParallelRotLineView.create(this)
+        LineBallSpreadRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
