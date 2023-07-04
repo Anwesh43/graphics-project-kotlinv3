ee7bdde423549204b617b3eae62f5c3e0b749fce
Anwesh43 <anweshthecool0@gmail.com>
2023-07-05 03:15:51 +0530

    balldroplinerotview: adding BallDropLineRotView class

--- balldroplinerotview/src/main/java/com/example/balldroplinerotview/BallDropLineRotView.kt
+++ balldroplinerotview/src/main/java/com/example/balldroplinerotview/BallDropLineRotView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawBDLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBallDropLineRot(scale, w, h, paint)
 }
+
+class BallDropLineRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
