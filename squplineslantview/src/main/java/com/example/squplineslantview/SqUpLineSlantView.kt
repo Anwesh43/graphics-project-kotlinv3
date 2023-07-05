0e4dccab784ceb2f74a2b8291393f6965f85ec36
Anwesh43 <anweshthecool0@gmail.com>
2023-07-06 01:33:58 +0530

    sqlineupslantview: adding Canvas extension methods

--- squplineslantview/src/main/java/com/example/squplineslantview/SqUpLineSlantView.kt
+++ squplineslantview/src/main/java/com/example/squplineslantview/SqUpLineSlantView.kt
@@ -18,14 +18,49 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
-val rot : Float = 90f
+val rot : Float = 45f
 val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqUpLineSlant(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2 + size) * dsc(4)) {
+        for (j in 0..1) {
+            drawXY(0f, h / 2 * (1 - dsc(3))) {
+                drawLine(0f, 0f, 0f, size, paint)
+            }
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(2))
+                drawLine(0f, 0f, 0f, size * dsc(0), paint)
+            }
+            drawRect(RectF(-size / 2, -size * dsc(1), size / 2, 0f), paint)
+        }
+    }
+}
+
+fun Canvas.drawSULSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSqUpLineSlant(scale, w, h, paint)
+}
