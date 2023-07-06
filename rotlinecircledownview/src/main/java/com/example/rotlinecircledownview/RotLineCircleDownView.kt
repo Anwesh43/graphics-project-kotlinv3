743d6097c13b1ba43a3622c3536da4070fa29306
Anwesh43 <anweshthecool0@gmail.com>
2023-07-07 03:32:56 +0530

    rotlinecricledownview: adding RLCDNode class

--- rotlinecircledownview/src/main/java/com/example/rotlinecircledownview/RotLineCircleDownView.kt
+++ rotlinecircledownview/src/main/java/com/example/rotlinecircledownview/RotLineCircleDownView.kt
@@ -125,4 +125,45 @@ class RotLineCircleDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RLCDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RLCDNode? = null
+        private var prev : RLCDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RLCDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRLCDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RLCDNode {
+            var curr : RLCDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
