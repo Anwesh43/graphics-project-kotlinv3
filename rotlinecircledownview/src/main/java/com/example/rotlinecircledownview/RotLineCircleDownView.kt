e8fbe777ff41d8e58ee4d7c6c93fdb11d9d6e1b7
Anwesh43 <anweshthecool0@gmail.com>
2023-07-07 03:06:49 +0530

    rotlinecricledownview: adding Canvas extension methods

--- rotlinecircledownview/src/main/java/com/example/rotlinecircledownview/RotLineCircleDownView.kt
+++ rotlinecircledownview/src/main/java/com/example/rotlinecircledownview/RotLineCircleDownView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 5
+val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -29,3 +29,36 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRotLineCircleDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(rot * (1 + dsc(2)) * j)
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+                drawArc(RectF(-size, -size / 2, 0f, size /2), 180f, 180f * dsc(1), true, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawRLCDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRotLineCircleDown(scale, w, h, paint)
+}
