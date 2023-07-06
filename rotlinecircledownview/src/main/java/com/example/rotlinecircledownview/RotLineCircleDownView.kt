ea7105fed02c4f6c35ef89d49483885d9f5cafd8
Anwesh43 <anweshthecool0@gmail.com>
2023-07-07 03:15:59 +0530

    rotlinecricledownview: adding RotLineCircleDownView class

--- rotlinecircledownview/src/main/java/com/example/rotlinecircledownview/RotLineCircleDownView.kt
+++ rotlinecircledownview/src/main/java/com/example/rotlinecircledownview/RotLineCircleDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawRLCDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRotLineCircleDown(scale, w, h, paint)
 }
+
+class RotLineCircleDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
