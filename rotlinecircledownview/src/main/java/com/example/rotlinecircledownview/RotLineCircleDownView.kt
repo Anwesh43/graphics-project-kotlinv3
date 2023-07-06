6097e60c62748d11bbeb02c97d5aef18bbed90a7
Anwesh43 <anweshthecool0@gmail.com>
2023-07-07 03:37:29 +0530

    rotlinecricledownview: adding Renderer class

--- rotlinecircledownview/src/main/java/com/example/rotlinecircledownview/RotLineCircleDownView.kt
+++ rotlinecircledownview/src/main/java/com/example/rotlinecircledownview/RotLineCircleDownView.kt
@@ -189,4 +189,27 @@ class RotLineCircleDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : RotLineCircleDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val rlc : RotLineCircleDown = RotLineCircleDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            rlc.draw(canvas, paint)
+            animator.animate {
+                rlc.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            rlc.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
