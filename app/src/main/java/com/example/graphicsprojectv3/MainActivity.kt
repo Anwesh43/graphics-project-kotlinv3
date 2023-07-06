5fd46f66381763c54d9e9e5b0e12519a0f19bd83
Anwesh43 <anweshthecool0@gmail.com>
2023-07-07 03:40:06 +0530

    rotlinecricledownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.squplineslantview.SqUpLineSlantView
+import com.example.rotlinecircledownview.RotLineCircleDownView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SqUpLineSlantView.create(this)
+        RotLineCircleDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
