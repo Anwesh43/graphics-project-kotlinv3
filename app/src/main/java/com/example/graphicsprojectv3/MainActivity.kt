8833e3205ca15c3e493a22caf3d3485509762102
Anwesh43 <anweshthecool0@gmail.com>
2023-07-08 02:28:44 +0530

    topsqdownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.rotlinecircledownview.RotLineCircleDownView
+import com.example.topsqdownview.TopSqDownView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        RotLineCircleDownView.create(this)
+        TopSqDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
