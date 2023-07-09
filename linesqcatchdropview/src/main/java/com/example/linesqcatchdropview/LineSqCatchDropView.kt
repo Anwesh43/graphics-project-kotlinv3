ecebff17fd7d756d78d8a33279cc1563a7fa48e7
Anwesh43 <anweshthecool0@gmail.com>
2023-07-10 02:25:18 +0530

    linesqcatchdropview: adding renderer in view

--- linesqcatchdropview/src/main/java/com/example/linesqcatchdropview/LineSqCatchDropView.kt
+++ linesqcatchdropview/src/main/java/com/example/linesqcatchdropview/LineSqCatchDropView.kt
@@ -65,14 +65,16 @@ fun Canvas.drawLSCDNode(i : Int, scale : Float, paint : Paint) {
 
 class LineSqCatchDropView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
