f3359a66d0158b9048e24ff1bc5050937f1a4721
Anwesh43 <anweshthecool0@gmail.com>
2023-07-10 02:42:36 +0530

    linesqcatchdropview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.topsqdownview.TopSqDownView
+import com.example.linesqcatchdropview.LineSqCatchDropView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        TopSqDownView.create(this)
+        LineSqCatchDropView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
