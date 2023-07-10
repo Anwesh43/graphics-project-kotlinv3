3b5f6cc2048ac7a2eedb9266c62a36a8fa7e2871
Anwesh43 <anweshthecool0@gmail.com>
2023-07-11 02:06:16 +0530

    linehalfcompletearcview: adding LHCANode class

--- linehalfcompletearcview/src/main/java/com/example/linehalfcompletearcview/LineHalfCompleteArcView.kt
+++ linehalfcompletearcview/src/main/java/com/example/linehalfcompletearcview/LineHalfCompleteArcView.kt
@@ -124,4 +124,45 @@ class LineHalfCompleteArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LHCANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LHCANode? = null
+        private var prev : LHCANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LHCANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLHCANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LHCANode {
+            var curr : LHCANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
