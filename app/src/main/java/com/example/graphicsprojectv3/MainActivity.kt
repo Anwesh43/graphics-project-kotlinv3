384eb66fea27a13f27481523b7918db900624746
Anwesh43 <anweshthecool0@gmail.com>
2023-07-11 02:11:42 +0530

    linehalfcompletearcview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linesqcatchdropview.LineSqCatchDropView
+import com.example.linehalfcompletearcview.LineHalfCompleteArcView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineSqCatchDropView.create(this)
+        LineHalfCompleteArcView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
