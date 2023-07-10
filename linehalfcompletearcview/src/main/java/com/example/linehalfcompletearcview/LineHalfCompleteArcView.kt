d9ae931083f3d6e5753ce993cec2139fb478469e
Anwesh43 <anweshthecool0@gmail.com>
2023-07-11 01:57:45 +0530

    linehalfcompletearcview: adding Canvas extension methods

--- linehalfcompletearcview/src/main/java/com/example/linehalfcompletearcview/LineHalfCompleteArcView.kt
+++ linehalfcompletearcview/src/main/java/com/example/linehalfcompletearcview/LineHalfCompleteArcView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -30,3 +30,34 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineHalfArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = size / 4
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        drawXY(size - r, 0f) {
+            drawArc(RectF(-r, -r, r, r), 0f, sweep * (dsc(1) + dsc(3)), true, paint)
+        }
+    }
+}
+
+fun Canvas.drawLHCANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineHalfArc(scale, w, h, paint)
+}
