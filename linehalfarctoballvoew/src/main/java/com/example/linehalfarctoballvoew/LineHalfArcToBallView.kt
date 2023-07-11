46060247c6900416ef7f3dc46b2340e764dae078
Anwesh43 <anweshthecool0@gmail.com>
2023-07-12 02:00:59 +0530

    linehalfarctoballview: adding Renderer class

--- linehalfarctoballvoew/src/main/java/com/example/linehalfarctoballvoew/LineHalfArcToBallView.kt
+++ linehalfarctoballvoew/src/main/java/com/example/linehalfarctoballvoew/LineHalfArcToBallView.kt
@@ -187,4 +187,27 @@ class LineHalfArcToBallView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineHalfArcToBallView) {
+
+        private val animator : Animator = Animator(view)
+        private val lhatb : LineHalfArcToBall = LineHalfArcToBall(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lhatb.draw(canvas, paint)
+            animator.animate {
+                lhatb.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lhatb.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
