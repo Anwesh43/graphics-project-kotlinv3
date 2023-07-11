b2904067511e6032f9073f9b2ce548a1e94a261f
Anwesh43 <anweshthecool0@gmail.com>
2023-07-12 01:54:02 +0530

    linehalfarctoballview: adding LHATBNode class

--- linehalfarctoballvoew/src/main/java/com/example/linehalfarctoballvoew/LineHalfArcToBallView.kt
+++ linehalfarctoballvoew/src/main/java/com/example/linehalfarctoballvoew/LineHalfArcToBallView.kt
@@ -123,4 +123,45 @@ class LineHalfArcToBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LHATBNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LHATBNode? = null
+        private var prev : LHATBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LHATBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLHATBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LHATBNode {
+            var curr : LHATBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
