c53711fa8d4576911bfec267d6af3dd9cb808dd3
Anwesh43 <anweshthecool0@gmail.com>
2023-07-12 01:43:07 +0530

    linehalfarctoballview: adding Canvas extension methods

--- linehalfarctoballvoew/src/main/java/com/example/linehalfarctoballvoew/LineHalfArcToBallView.kt
+++ linehalfarctoballvoew/src/main/java/com/example/linehalfarctoballvoew/LineHalfArcToBallView.kt
@@ -28,3 +28,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineHalfArcToBall(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = size / 5
+    drawXY(w / 2, h / 2 + (h / 2 + r) * dsc(3)) {
+        for (j in 0..1) {
+            drawXY((size - r) * (1 - 2 * j) * (1 - dsc(2)), 0f) {
+                drawArc(RectF(-r, -r, r, r), 180f * j, 180f * dsc(1), true, paint)
+            }
+            drawLine(0f, 0f, size * (1f - 2 * j) * (1 - dsc(2)), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLHATBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineHalfArcToBall(scale, w, h, paint)
+}
