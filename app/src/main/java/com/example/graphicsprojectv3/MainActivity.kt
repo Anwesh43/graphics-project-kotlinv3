4502f0f864d2cdf79c39a2758b66b36cd59ff82a
Anwesh43 <anweshthecool0@gmail.com>
2023-07-12 02:02:26 +0530

    linehalfarctoballview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linehalfcompletearcview.LineHalfCompleteArcView
+import com.example.linehalfarctoballvoew.LineHalfArcToBallView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineHalfCompleteArcView.create(this)
+        LineHalfArcToBallView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
