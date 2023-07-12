4681da4f331d74ade249dd5dc171212f05d823c1
Anwesh43 <anweshthecool0@gmail.com>
2023-07-13 03:12:47 +0530

    arclinetridownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linehalfarctoballvoew.LineHalfArcToBallView
+import com.example.arclinetridownview.ArcTriLineDownView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineHalfArcToBallView.create(this)
+        ArcTriLineDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
