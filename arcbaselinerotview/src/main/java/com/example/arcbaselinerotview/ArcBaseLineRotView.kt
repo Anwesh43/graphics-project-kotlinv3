5468851cebada14827c5bed2640040db814ed146
Anwesh43 <anweshthecool0@gmail.com>
2023-07-14 02:39:02 +0530

    arcbaselinerotview: adding ArcBaseLineRot class

--- arcbaselinerotview/src/main/java/com/example/arcbaselinerotview/ArcBaseLineRotView.kt
+++ arcbaselinerotview/src/main/java/com/example/arcbaselinerotview/ArcBaseLineRotView.kt
@@ -169,4 +169,27 @@ class ArcBaseLineRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcBaseLineRot(var i : Int) {
+
+        private var curr : ABLRNode = ABLRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
