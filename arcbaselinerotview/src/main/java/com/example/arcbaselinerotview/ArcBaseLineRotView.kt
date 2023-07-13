967f5f35a280e6a83e3df8c23e3eee9d25ab6b91
Anwesh43 <anweshthecool0@gmail.com>
2023-07-14 02:41:56 +0530

    arcbaselinerotview: adding Renderer class

--- arcbaselinerotview/src/main/java/com/example/arcbaselinerotview/ArcBaseLineRotView.kt
+++ arcbaselinerotview/src/main/java/com/example/arcbaselinerotview/ArcBaseLineRotView.kt
@@ -192,4 +192,27 @@ class ArcBaseLineRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcBaseLineRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val ablr : ArcBaseLineRot = ArcBaseLineRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ablr.draw(canvas, paint)
+            animator.animate {
+                ablr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ablr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
