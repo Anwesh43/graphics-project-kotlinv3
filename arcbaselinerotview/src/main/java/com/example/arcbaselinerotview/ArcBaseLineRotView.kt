518e4d0245096153b51762a03c48467094600cfa
Anwesh43 <anweshthecool0@gmail.com>
2023-07-14 02:26:27 +0530

    arcbaselinerotview: adding ArcBaseLineRotView class

--- arcbaselinerotview/src/main/java/com/example/arcbaselinerotview/ArcBaseLineRotView.kt
+++ arcbaselinerotview/src/main/java/com/example/arcbaselinerotview/ArcBaseLineRotView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawABLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcBaseLineRot(scale, w, h, paint)
 }
+
+class ArcBaseLineRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
