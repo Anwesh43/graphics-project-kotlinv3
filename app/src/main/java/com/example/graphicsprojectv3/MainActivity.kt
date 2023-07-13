6f43462710c9baf692f16931b5b06d237fe9a8c1
Anwesh43 <anweshthecool0@gmail.com>
2023-07-14 02:45:44 +0530

    arcbaselinerotview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.arclinetridownview.ArcTriLineDownView
+import com.example.arcbaselinerotview.ArcBaseLineRotView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        ArcTriLineDownView.create(this)
+        ArcBaseLineRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
