ebcddf258e494e5cfeaee35b37b706dfc582c6d0
Anwesh43 <anweshthecool0@gmail.com>
2023-07-15 11:01:39 +0530

    linerotsqexpandview: adding LRSENode class

--- linerotsqexpandview/src/main/java/com/example/linerotsqexpandview/LineRotSqExpandView.kt
+++ linerotsqexpandview/src/main/java/com/example/linerotsqexpandview/LineRotSqExpandView.kt
@@ -99,4 +99,45 @@ class LineRotSqExpandView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRSENode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LRSENode? = null
+        private var next : LRSENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRSENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRSENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRSENode {
+            var curr : LRSENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
