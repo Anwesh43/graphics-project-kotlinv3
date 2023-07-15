57b2c72948849029a30489166b938ed794fb34b0
Anwesh43 <anweshthecool0@gmail.com>
2023-07-15 10:53:33 +0530

    linerotsqexpandview: adding Canvas extension methods

--- linerotsqexpandview/src/main/java/com/example/linerotsqexpandview/LineRotSqExpandView.kt
+++ linerotsqexpandview/src/main/java/com/example/linerotsqexpandview/LineRotSqExpandView.kt
@@ -29,3 +29,38 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotSqExpand(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                drawXY(0f, 0f) {
+                    rotate(-rot * dsc(1))
+                    drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+                }
+                drawRect(RectF(0f, 0f, size * dsc(2), size / 2), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLRSENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotSqExpand(scale, w, h, paint)
+}
