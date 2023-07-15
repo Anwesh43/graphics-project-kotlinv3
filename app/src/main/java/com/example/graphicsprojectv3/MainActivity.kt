df6184b2d02b8f785ab0fe9f63353bd195007a94
Anwesh43 <anweshthecool0@gmail.com>
2023-07-15 11:38:58 +0530

    linerotsqexpandview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.arcbaselinerotview.ArcBaseLineRotView
+import com.example.linerotsqexpandview.LineRotSqExpandView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        ArcBaseLineRotView.create(this)
+        LineRotSqExpandView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
