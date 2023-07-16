a5c37284217af52667e4aa192d1d4a28d5b0fa92
Anwesh43 <anweshthecool0@gmail.com>
2023-07-16 09:52:40 +0530

    linedroplview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotsqexpandview.LineRotSqExpandView
+import com.example.linedroplview.LineDropLView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineRotSqExpandView.create(this)
+        LineDropLView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
