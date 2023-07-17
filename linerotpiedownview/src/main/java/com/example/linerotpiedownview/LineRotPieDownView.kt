8e33163f31e87a193cbb88fcc549986f4019e55f
Anwesh43 <anweshthecool0@gmail.com>
2023-07-17 10:26:14 +0530

    linerotpiedownview: adding LRPDNode class

--- linerotpiedownview/src/main/java/com/example/linerotpiedownview/LineRotPieDownView.kt
+++ linerotpiedownview/src/main/java/com/example/linerotpiedownview/LineRotPieDownView.kt
@@ -129,4 +129,45 @@ class LineRotPieDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRPDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRPDNode? = null
+        private var prev : LRPDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size -1) {
+                next = LRPDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRPDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRPDNode {
+            var curr : LRPDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
