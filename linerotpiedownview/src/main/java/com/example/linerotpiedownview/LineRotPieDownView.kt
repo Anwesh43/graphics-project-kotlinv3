5eed249526cdf19337abd332dc37e196eed7a776
Anwesh43 <anweshthecool0@gmail.com>
2023-07-17 09:42:18 +0530

    linerotpiedownview: adding Int and Float extension methods

--- linerotpiedownview/src/main/java/com/example/linerotpiedownview/LineRotPieDownView.kt
+++ linerotpiedownview/src/main/java/com/example/linerotpiedownview/LineRotPieDownView.kt
@@ -21,8 +21,12 @@ val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 45f
 val sizeFactor : Float = 4.9f
 val strokeFactor : Float = 90f
 
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
