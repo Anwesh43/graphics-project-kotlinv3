b2ef95b30f4355f36b1e51193c30d500477de033
Anwesh43 <anweshthecool0@gmail.com>
2023-07-18 09:57:34 +0530

    linebaserotupview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotpiedownview.LineRotPieDownView
+import com.example.linebaserotupview.LineBaseRotUpView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineRotPieDownView.create(this)
+        LineBaseRotUpView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
