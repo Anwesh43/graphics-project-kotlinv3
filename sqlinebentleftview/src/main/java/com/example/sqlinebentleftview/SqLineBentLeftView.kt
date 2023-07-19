31f22bd98274977d6ba54ab592f122ac10001905
Anwesh43 <anweshthecool0@gmail.com>
2023-07-19 10:15:02 +0530

    sqlinebentleftview: adding Canvas extension methods

--- sqlinebentleftview/src/main/java/com/example/sqlinebentleftview/SqLineBentLeftView.kt
+++ sqlinebentleftview/src/main/java/com/example/sqlinebentleftview/SqLineBentLeftView.kt
@@ -30,3 +30,36 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqLineBentLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 + size) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        drawXY(0f, -h / 2 + (h / 2) * dsc(1)) {
+            rotate(deg * dsc(2))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        drawXY((-w / 2 - size /2) * (1 - dsc(0)), 0f) {
+            drawRect(RectF(-size / 2, -size / 2, size / 2, size / 2), paint)
+        }
+    }
+}
+
+fun Canvas.draSLBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSqLineBentLeft(scale, w, h, paint)
+}
