80fcfc0294b936747f0b6dcf3e3cdbac5f1d6d20
Anwesh43 <anweshthecool0@gmail.com>
2023-07-19 10:42:03 +0530

    sqlinebentleftview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linebaserotupview.LineBaseRotUpView
+import com.example.sqlinebentleftview.SqLineBentLeftView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineBaseRotUpView.create(this)
+        SqLineBentLeftView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
