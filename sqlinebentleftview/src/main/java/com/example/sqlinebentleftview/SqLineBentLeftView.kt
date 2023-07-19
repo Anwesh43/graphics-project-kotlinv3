1ef0aa2afa3db98400de4fd008e0a257487c2e5e
Anwesh43 <anweshthecool0@gmail.com>
2023-07-19 10:38:25 +0530

    sqlinebentleftview: adding SqLineBentLine class

--- sqlinebentleftview/src/main/java/com/example/sqlinebentleftview/SqLineBentLeftView.kt
+++ sqlinebentleftview/src/main/java/com/example/sqlinebentleftview/SqLineBentLeftView.kt
@@ -168,4 +168,27 @@ class SqLineBentLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SqLineBentLeft(var i : Int = 0) {
+
+        private var curr : SLBLNode = SLBLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
