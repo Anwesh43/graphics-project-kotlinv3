5d8dd923c8e1c35bdd86bab7ca9fe33aa9a69e37
Anwesh43 <anweshthecool0@gmail.com>
2023-07-20 10:13:26 +0530

    boxtakecrosslineview: adding fixes

--- boxtakecrosslineview/src/main/java/com/example/boxtakecrosslineview/BoxTakeCrossLineView.kt
+++ boxtakecrosslineview/src/main/java/com/example/boxtakecrosslineview/BoxTakeCrossLineView.kt
@@ -42,7 +42,7 @@ fun Canvas.drawBoxTakeCrossLine(scale : Float, w : Float, h : Float, paint : Pai
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
         drawRect(RectF(0f, -size / 2, size * dsc(0), size / 2), paint)
         for (j in 0..1) {
             drawXY((-w / 2) * (1 - dsc(1)), 0f) {
