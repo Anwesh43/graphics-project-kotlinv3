5e71b63e8424111ba291136f592b8477d941f88a
Anwesh43 <anweshthecool0@gmail.com>
2023-07-20 09:32:43 +0530

    boxtakecrosslineview: adding BoxTakeCrossLineView class

--- boxtakecrosslineview/src/main/java/com/example/boxtakecrosslineview/BoxTakeCrossLineView.kt
+++ boxtakecrosslineview/src/main/java/com/example/boxtakecrosslineview/BoxTakeCrossLineView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawBTCLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBoxTakeCrossLine(scale, w, h, paint)
 }
+
+class BoxTakeCrossLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
