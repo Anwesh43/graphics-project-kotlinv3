220a5b2acd6926bfecf20b7e64b6fce0c90c1cde
Anwesh43 <anweshthecool0@gmail.com>
2023-07-20 10:12:07 +0530

    boxtakecrosslineview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqlinebentleftview.SqLineBentLeftView
+import com.example.boxtakecrosslineview.BoxTakeCrossLineView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SqLineBentLeftView.create(this)
+        BoxTakeCrossLineView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
