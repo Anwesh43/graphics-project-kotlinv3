ad1fff2ad32e54e818a028e650717c2e91cbb5fa
Anwesh43 <anweshthecool0@gmail.com>
2023-07-20 10:06:10 +0530

    boxtakecrosslineview: adding BoxTakeCrossLine class

--- boxtakecrosslineview/src/main/java/com/example/boxtakecrosslineview/BoxTakeCrossLineView.kt
+++ boxtakecrosslineview/src/main/java/com/example/boxtakecrosslineview/BoxTakeCrossLineView.kt
@@ -168,4 +168,27 @@ class BoxTakeCrossLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BoxTakeCrossLine(var i : Int) {
+
+        private var curr : BTCLNode = BTCLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+           curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
