fe65346dad3cbe696325c3e1fe0bc0fcc7d25086
Anwesh43 <anweshthecool0@gmail.com>
2023-07-21 06:17:17 +0530

    rothalflinerightview: adding RotHalfLineRight class

--- rothalflinerightview/src/main/java/com/example/rothalflinerightview/RotHalfLineRightView.kt
+++ rothalflinerightview/src/main/java/com/example/rothalflinerightview/RotHalfLineRightView.kt
@@ -171,4 +171,27 @@ class RotHalfLineRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RotHalfLineRight(var i : Int) {
+
+        private var curr : RHLRNode = RHLRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
