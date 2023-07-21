f80f91b14321ecaa932e299c20103d4ba63eb7f8
Anwesh43 <anweshthecool0@gmail.com>
2023-07-21 06:01:34 +0530

    rothalflinerightview: adding RotHalfLineRightView class

--- rothalflinerightview/src/main/java/com/example/rothalflinerightview/RotHalfLineRightView.kt
+++ rothalflinerightview/src/main/java/com/example/rothalflinerightview/RotHalfLineRightView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawRHLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRotHalfLineRight(scale, w, h, paint)
 }
+
+class RotHalfLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
