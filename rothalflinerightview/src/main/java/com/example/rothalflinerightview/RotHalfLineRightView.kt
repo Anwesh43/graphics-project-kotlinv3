fa9038a1f575030eb9904a731b7aa7298d418a82
Anwesh43 <anweshthecool0@gmail.com>
2023-07-21 06:08:44 +0530

    rothalflinerightview: adding RHLRNode class

--- rothalflinerightview/src/main/java/com/example/rothalflinerightview/RotHalfLineRightView.kt
+++ rothalflinerightview/src/main/java/com/example/rothalflinerightview/RotHalfLineRightView.kt
@@ -130,4 +130,45 @@ class RotHalfLineRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RHLRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RHLRNode? = null
+        private var prev : RHLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RHLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRHLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RHLRNode {
+            var curr : RHLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
