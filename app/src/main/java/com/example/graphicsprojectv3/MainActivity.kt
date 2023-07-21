e70c13d8858c77057024d2fb069b6349037433ca
Anwesh43 <anweshthecool0@gmail.com>
2023-07-21 06:21:23 +0530

    rothalflinerightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.boxtakecrosslineview.BoxTakeCrossLineView
+import com.example.rothalflinerightview.RotHalfLineRightView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BoxTakeCrossLineView.create(this)
+        RotHalfLineRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
