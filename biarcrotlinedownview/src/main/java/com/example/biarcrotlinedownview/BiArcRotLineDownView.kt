39543b4c99839e1090c120f1b8d3c8e111795efe
Anwesh43 <anweshthecool0@gmail.com>
2023-07-22 09:25:51 +0530

    biarcrotlinedowview: adding Renderer class

--- biarcrotlinedownview/src/main/java/com/example/biarcrotlinedownview/BiArcRotLineDownView.kt
+++ biarcrotlinedownview/src/main/java/com/example/biarcrotlinedownview/BiArcRotLineDownView.kt
@@ -190,4 +190,27 @@ class BiArcRotLineDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiArcRotLineDownView) {
+
+        private val barld : BiArcRotLineDown = BiArcRotLineDown(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            barld.draw(canvas, paint)
+            animator.animate {
+                barld.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            barld.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
