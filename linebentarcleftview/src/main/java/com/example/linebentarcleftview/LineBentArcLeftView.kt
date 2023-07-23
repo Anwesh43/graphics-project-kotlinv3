ab6b6877342ada047705977543aa88deb73e72d8
Anwesh43 <anweshthecool0@gmail.com>
2023-07-23 12:32:35 +0530

    linebentarcleftview: adding Renderer class

--- linebentarcleftview/src/main/java/com/example/linebentarcleftview/LineBentArcLeftView.kt
+++ linebentarcleftview/src/main/java/com/example/linebentarcleftview/LineBentArcLeftView.kt
@@ -190,4 +190,27 @@ class LineBentArcLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBentArcLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val lbal : LineBentArcLeft = LineBentArcLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbal.draw(canvas, paint)
+            animator.animate {
+                lbal.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbal.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
