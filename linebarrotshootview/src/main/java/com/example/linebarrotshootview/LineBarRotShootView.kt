e05b482f169b02f79641cb3953955835b2ec9184
Anwesh43 <anweshthecool0@gmail.com>
2023-07-24 14:05:59 +0530

    linebarrrotshootview: adding LBRSNode class

--- linebarrotshootview/src/main/java/com/example/linebarrotshootview/LineBarRotShootView.kt
+++ linebarrotshootview/src/main/java/com/example/linebarrotshootview/LineBarRotShootView.kt
@@ -130,4 +130,45 @@ class LineBarRotShootView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBRSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBRSNode? = null
+        private var prev : LBRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBRSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBRSNode {
+            var curr : LBRSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
