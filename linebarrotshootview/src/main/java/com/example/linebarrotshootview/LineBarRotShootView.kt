e0e824e68af82e2e8bc342afd443a978d03010f7
Anwesh43 <anweshthecool0@gmail.com>
2023-07-24 11:19:44 +0530

    linebarrrotshootview: adding LineBarRotShootView class

--- linebarrotshootview/src/main/java/com/example/linebarrotshootview/LineBarRotShootView.kt
+++ linebarrotshootview/src/main/java/com/example/linebarrotshootview/LineBarRotShootView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawLBRSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBarRotShoot(scale, w, h, paint)
 }
+
+class LineBarRotShootView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
