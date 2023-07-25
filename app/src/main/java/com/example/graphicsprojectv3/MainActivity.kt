f26d77d5e9729c69955cb968a1cf97a35d207f92
Anwesh43 <anweshthecool0@gmail.com>
2023-07-25 11:54:55 +0530

    linesqtakerightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linebarrotshootview.LineBarRotShootView
+import com.example.linesqtakerightview.LineSqTakeRightView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineBarRotShootView.create(this)
+        LineSqTakeRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
