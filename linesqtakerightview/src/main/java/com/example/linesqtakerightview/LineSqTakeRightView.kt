143cb6d19c26292a63c82e477749c173bbf9bc06
Anwesh43 <anweshthecool0@gmail.com>
2023-07-25 11:34:53 +0530

    linesqtakerightview: adding LineSqTakeRight class

--- linesqtakerightview/src/main/java/com/example/linesqtakerightview/LineSqTakeRightView.kt
+++ linesqtakerightview/src/main/java/com/example/linesqtakerightview/LineSqTakeRightView.kt
@@ -167,4 +167,27 @@ class LineSqTakeRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineSqTakeRight(var i : Int) {
+
+        private var curr : LSTRNode = LSTRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
