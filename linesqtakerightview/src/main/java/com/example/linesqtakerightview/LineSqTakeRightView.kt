e2ce8c8a022019c1578817568d44582ff7970f79
Anwesh43 <anweshthecool0@gmail.com>
2023-07-25 11:54:10 +0530

    linesqtakerightview: adding static create method

--- linesqtakerightview/src/main/java/com/example/linesqtakerightview/LineSqTakeRightView.kt
+++ linesqtakerightview/src/main/java/com/example/linesqtakerightview/LineSqTakeRightView.kt
@@ -215,4 +215,13 @@ class LineSqTakeRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineSqTakeRightView {
+            val view : LineSqTakeRightView = LineSqTakeRightView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
