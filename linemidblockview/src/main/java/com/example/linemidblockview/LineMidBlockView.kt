0c2d977455e3b4809d7d2b6167ef8a7c6cd7bb55
Anwesh43 <anweshthecool0@gmail.com>
2023-07-26 11:18:59 +0530

    linesqtakerightview: adding LineMidBlockView class

--- linemidblockview/src/main/java/com/example/linemidblockview/LineMidBlockView.kt
+++ linemidblockview/src/main/java/com/example/linemidblockview/LineMidBlockView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawLMBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineMidBlock(scale, w, h, paint)
 }
+
+class LineMidBlockView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
