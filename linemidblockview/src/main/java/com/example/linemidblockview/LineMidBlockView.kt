9496d66e564fc1a41f4c6b1d92e8cdca6cda4117
Anwesh43 <anweshthecool0@gmail.com>
2023-07-26 11:46:04 +0530

    linesqtakerightview: adding LMTNode class

--- linemidblockview/src/main/java/com/example/linemidblockview/LineMidBlockView.kt
+++ linemidblockview/src/main/java/com/example/linemidblockview/LineMidBlockView.kt
@@ -127,4 +127,45 @@ class LineMidBlockView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LMBNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LMBNode? = null
+        private var prev : LMBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LMBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLMBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LMBNode {
+            var curr : LMBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
