78e6d0a41c1f03a5560f82ddc43e0f6b72375527
Anwesh43 <anweshthecool0@gmail.com>
2023-07-27 11:15:01 +0530

    lineslantrightsqview: adding constants and libraries

---
+++ lineslantrightsqview/src/main/java/com/example/lineslantrightsqview/LineSlantRightSqView.kt
@@ -0,0 +1,28 @@
+package com.example.lineslantrightsqview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f
+val strokeFactor : Float = 90f
+val rot : Float = 45f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val sizeFactor : Float = 4.9f
+
