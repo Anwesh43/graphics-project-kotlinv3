dcc9ea3883d58a50a29c5ae0ad6dffd5460c94f5
Anwesh43 <anweshthecool0@gmail.com>
2023-07-28 01:45:37 +0530

    lineexpandfromsingleview: adding Canvas extension methods

--- lineexpandfromsingleview/src/main/java/com/example/lineexpandfromsingleview/LineExpandFromSingleView.kt
+++ lineexpandfromsingleview/src/main/java/com/example/lineexpandfromsingleview/LineExpandFromSingleView.kt
@@ -23,8 +23,40 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineExpandFromSingle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY((w / 2) * (1 + dsc(3)), h / 2) {
+        drawLine(0f, 0f, -size * (dsc(0) - dsc(1)), 0f, paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * (1f - 2 * j) * (1 - dsc(2)))
+                drawLine(0f, 0f, size * dsc(1), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLEFSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineExpandFromSingle(scale,w , h, paint)
+}
