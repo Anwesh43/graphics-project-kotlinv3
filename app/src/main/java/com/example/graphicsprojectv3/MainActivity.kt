683ac82161718d3c8a3dedd3e70e8d1b8e18666b
Anwesh43 <anweshthecool0@gmail.com>
2023-07-28 01:55:49 +0530

    lineexpandfromsingleview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineslantrightsqview.LineSlantRightSqView
+import com.example.lineexpandfromsingleview.LineExpandFromSingleView
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineSlantRightSqView.create(this)
+        LineExpandFromSingleView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
