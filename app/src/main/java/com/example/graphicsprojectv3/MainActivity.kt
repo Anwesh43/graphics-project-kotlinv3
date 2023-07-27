af174770e64ff8a53d87456f70b633980b2dea59
Anwesh43 <anweshthecool0@gmail.com>
2023-07-27 13:16:37 +0530

    lineslantrightsqview: adding static create method

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linemidblockview.LineMidBlockView
+import com.example.lineslantrightsqview.LineSlantRightSqView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineMidBlockView.create(this)
+        LineSlantRightSqView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
