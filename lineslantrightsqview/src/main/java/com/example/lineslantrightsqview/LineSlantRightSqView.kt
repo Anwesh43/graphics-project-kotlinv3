608fc109cb72e87e0f1eb4bc285360ac5db43902
Anwesh43 <anweshthecool0@gmail.com>
2023-07-27 11:53:45 +0530

    lineslantrightsqview: adding LineSlantRightSqView class

--- lineslantrightsqview/src/main/java/com/example/lineslantrightsqview/LineSlantRightSqView.kt
+++ lineslantrightsqview/src/main/java/com/example/lineslantrightsqview/LineSlantRightSqView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLSRSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSlantRightSq(scale, w, h, paint)
 }
+
+class LineSlantRightSqView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
