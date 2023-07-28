7ae70e323e2b36bf08be99846ab16af93eb2a90f
Anwesh43 <anweshthecool0@gmail.com>
2023-07-29 01:10:30 +0530

    linerotperpupview: adding Canvas extension methods

--- linerotperpupview/src/main/java/com/example/linerotperpupview/LineRotPerpUpView.kt
+++ linerotperpupview/src/main/java/com/example/linerotperpupview/LineRotPerpUpView.kt
@@ -28,3 +28,34 @@ val rot : Float = 270f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i / n)
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotPerpUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2 + size) * dsc(3)) {
+        for (j in 0..1) {
+            drawXY((size / 2) * dsc(2) * j, 0f) {
+                rotate(-rot * j * dsc(1))
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLRPUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotPerpUp(scale, w, h, paint)
+}
