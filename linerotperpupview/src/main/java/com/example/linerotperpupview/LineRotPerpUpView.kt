69a153e84a974ca24bd23e790e589abfca351241
Anwesh43 <anweshthecool0@gmail.com>
2023-07-29 01:11:49 +0530

    linerotperpupview: adding LineRotPerpUpView class

--- linerotperpupview/src/main/java/com/example/linerotperpupview/LineRotPerpUpView.kt
+++ linerotperpupview/src/main/java/com/example/linerotperpupview/LineRotPerpUpView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLRPUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotPerpUp(scale, w, h, paint)
 }
+
+class LineRotPerpUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
