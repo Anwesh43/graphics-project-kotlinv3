efbaa78ba616ba839ef277114c628d69d238e92d
Anwesh43 <anweshthecool0@gmail.com>
2023-07-30 17:10:44 +0530

    sqslantrightview: adding SqSlantLineRightView class

--- sqslantlinerightview/src/main/java/com/example/sqslantlinerightview/SqSlantLineRightView.kt
+++ sqslantlinerightview/src/main/java/com/example/sqslantlinerightview/SqSlantLineRightView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawSSLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSqSlantLineRight(scale, w, h, paint)
 }
+
+class SqSlantLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
