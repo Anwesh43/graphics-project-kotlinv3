9a57e01ffe1e783e14c33919dc7d069e5d1f8b52
Anwesh43 <anweshthecool0@gmail.com>
2023-07-30 17:53:34 +0530

    sqslantrightview: adding fixes

--- sqslantlinerightview/src/main/java/com/example/sqslantlinerightview/SqSlantLineRightView.kt
+++ sqslantlinerightview/src/main/java/com/example/sqslantlinerightview/SqSlantLineRightView.kt
@@ -24,7 +24,7 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 45f
+val rot : Float = 135f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -38,14 +38,15 @@ fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
 }
 
 fun Canvas.drawSqSlantLineRight(scale : Float, w : Float, h : Float, paint : Paint) {
-    val size : Float = Math.min(w, h) / strokeFactor
+    val size : Float = Math.min(w, h) / sizeFactor
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
         drawRect(RectF(-size, -size * dsc(1), size, 0f), paint)
         for (j in 0..1) {
             drawXY(-size + size * j, 0f) {
+                rotate(rot * dsc(0))
                 drawLine(0f, 0f, size * dsc(0), 0f, paint)
             }
         }
