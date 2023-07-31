6bad5786a92cf125aa7bd924a10a499a0527b263
Anwesh43 <anweshthecool0@gmail.com>
2023-07-31 11:56:48 +0530

    bilinerotsqrightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqslantlinerightview.SqSlantLineRightView
+import com.example.bilinerotsqrightview.BiLineRotSqRightView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SqSlantLineRightView.create(this)
+        BiLineRotSqRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
