2a22cea0f843850b2142c1900100f0717423db2e
Anwesh43 <anweshthecool0@gmail.com>
2023-07-31 11:50:19 +0530

    bilinerotsqrightview: adding BiLineRotSqRight class

--- bilinerotsqrightview/src/main/java/com/example/bilinerotsqrightview/BiLineRotSqRightView.kt
+++ bilinerotsqrightview/src/main/java/com/example/bilinerotsqrightview/BiLineRotSqRightView.kt
@@ -166,4 +166,27 @@ class BiLineRotSqRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLineRotSqRight(var i : Int) {
+
+        private var curr : BLRSRNode = BLRSRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
