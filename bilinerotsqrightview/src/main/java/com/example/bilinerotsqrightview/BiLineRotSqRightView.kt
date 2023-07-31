a017e6f6dd9b2ee8a96a382c28fb046085c988da
Anwesh43 <anweshthecool0@gmail.com>
2023-07-31 11:48:05 +0530

    bilinerotsqrightview: adding BLRSRNode class

--- bilinerotsqrightview/src/main/java/com/example/bilinerotsqrightview/BiLineRotSqRightView.kt
+++ bilinerotsqrightview/src/main/java/com/example/bilinerotsqrightview/BiLineRotSqRightView.kt
@@ -125,4 +125,45 @@ class BiLineRotSqRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLRSRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLRSRNode? = null
+        private var prev : BLRSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLRSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLRSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLRSRNode {
+            var curr : BLRSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
