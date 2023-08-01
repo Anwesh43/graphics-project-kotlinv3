dc621d78c71e22b94982fa23906bdc719539bd5c
Anwesh43 <anweshthecool0@gmail.com>
2023-08-01 11:17:10 +0530

    linehalfarcrotview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.bilinerotsqrightview.BiLineRotSqRightView
+import com.example.linehalfarcrotview.LineHalfArcRotView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BiLineRotSqRightView.create(this)
+        LineHalfArcRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
