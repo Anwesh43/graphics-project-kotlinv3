3582a1c9ba89d60b890a95451f6f9263e788a554
Anwesh43 <anweshthecool0@gmail.com>
2023-08-01 11:02:33 +0530

    linehalfarcrotview: adding LHARNode class

--- linehalfarcrotview/src/main/java/com/example/linehalfarcrotview/LineHalfArcRotView.kt
+++ linehalfarcrotview/src/main/java/com/example/linehalfarcrotview/LineHalfArcRotView.kt
@@ -121,4 +121,45 @@ class LineHalfArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LHARNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LHARNode? = null
+        private var next : LHARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LHARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLHARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LHARNode {
+            var curr : LHARNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
