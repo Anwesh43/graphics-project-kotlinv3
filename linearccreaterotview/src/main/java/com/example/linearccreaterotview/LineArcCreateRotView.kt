cfac8da965d34e222e6b08fc36c7e5c0c9adc496
Anwesh43 <anweshthecool0@gmail.com>
2023-08-02 09:36:34 +0530

    linearccreaterotview: adding LineArcCreateRotView class

--- linearccreaterotview/src/main/java/com/example/linearccreaterotview/LineArcCreateRotView.kt
+++ linearccreaterotview/src/main/java/com/example/linearccreaterotview/LineArcCreateRotView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLACRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineArcCreateRot(scale, w, h, paint)
 }
+
+class LineArcCreateRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
