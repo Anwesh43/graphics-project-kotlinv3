20882565ac415684089deb74b793bb177bf22e0a
Anwesh43 <anweshthecool0@gmail.com>
2023-08-02 09:20:25 +0530

    linearccreaterotview: adding Canvas extension methods

--- linearccreaterotview/src/main/java/com/example/linearccreaterotview/LineArcCreateRotView.kt
+++ linearccreaterotview/src/main/java/com/example/linearccreaterotview/LineArcCreateRotView.kt
@@ -29,3 +29,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcCreateRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val sweep : Float = 180f * (1 - dsc(1))
+    drawXY(w / 2, h / 2) {
+        drawLine(-size, 0f, -size + size * dsc(0), 0f, paint)
+        drawXY(0f, 0f) {
+            rotate(-180f * dsc(2))
+            drawArc(RectF(0f, -size / 2, size, size / 2), sweep, sweep, false, paint)
+        }
+    }
+}
+
+fun Canvas.drawLACRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineArcCreateRot(scale, w, h, paint)
+}
