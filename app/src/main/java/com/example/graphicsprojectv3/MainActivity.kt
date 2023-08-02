114cf323bb512d3a8b9d3a364e03c6a107f639fe
Anwesh43 <anweshthecool0@gmail.com>
2023-08-02 10:08:20 +0530

    linearccreaterotview: adding view in actvivity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linehalfarcrotview.LineHalfArcRotView
+import com.example.linearccreaterotview.LineArcCreateRotView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineHalfArcRotView.create(this)
+        LineArcCreateRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
