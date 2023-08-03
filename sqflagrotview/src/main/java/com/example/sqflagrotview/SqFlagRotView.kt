8cbfc74bd551b19bcea88c6321566860f35c0a94
Anwesh43 <anweshthecool0@gmail.com>
2023-08-04 01:25:22 +0530

    sqflgrotview: adding SqFlagRotView class

--- sqflagrotview/src/main/java/com/example/sqflagrotview/SqFlagRotView.kt
+++ sqflagrotview/src/main/java/com/example/sqflagrotview/SqFlagRotView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawSFRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
 }
+
+class SqFlagRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
