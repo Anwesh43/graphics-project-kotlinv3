7401815e05f0d3b045d4ecda8b7ebd3e39aca60a
Anwesh43 <anweshthecool0@gmail.com>
2023-08-04 01:39:48 +0530

    sqflgrotview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerottriupview.LineRotTriUpView
+import com.example.sqflagrotview.SqFlagRotView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineRotTriUpView.create(this)
+        SqFlagRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
