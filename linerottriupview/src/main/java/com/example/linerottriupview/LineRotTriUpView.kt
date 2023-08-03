a4c129fa1a511e71729650f077a9fc8ba9b04921
Anwesh43 <anweshthecool0@gmail.com>
2023-08-03 12:37:26 +0530

    linerottriupview: adding LRTUNode class

--- linerottriupview/src/main/java/com/example/linerottriupview/LineRotTriUpView.kt
+++ linerottriupview/src/main/java/com/example/linerottriupview/LineRotTriUpView.kt
@@ -123,4 +123,45 @@ class LineRotTriUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRTUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRTUNode? = null
+        private var prev : LRTUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRTUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRTUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRTUNode {
+            var curr : LRTUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
