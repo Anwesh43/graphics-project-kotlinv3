a2b0adc795dc09875bf320822eeea939b645e4b8
Anwesh43 <anweshthecool0@gmail.com>
2023-08-04 01:33:19 +0530

    sqflgrotview: adding SFRNode class

--- sqflagrotview/src/main/java/com/example/sqflagrotview/SqFlagRotView.kt
+++ sqflagrotview/src/main/java/com/example/sqflagrotview/SqFlagRotView.kt
@@ -122,4 +122,45 @@ class SqFlagRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SFRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SFRNode? = null
+        private var prev : SFRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SFRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSFRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SFRNode {
+            var curr : SFRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
