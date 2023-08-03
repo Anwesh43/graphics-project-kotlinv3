d7b409aedf4136b34b87974fee1533a96c34aae5
Anwesh43 <anweshthecool0@gmail.com>
2023-08-03 12:43:36 +0530

    linerottriupview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linearccreaterotview.LineArcCreateRotView
+import com.example.linerottriupview.LineRotTriUpView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineArcCreateRotView.create(this)
+        LineRotTriUpView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
