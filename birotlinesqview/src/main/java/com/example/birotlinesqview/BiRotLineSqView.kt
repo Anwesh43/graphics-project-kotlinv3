d628c5ae2fb0578993b1d57b26d6649cc550fc62
Anwesh43 <anweshthecool0@gmail.com>
2023-08-05 10:42:28 +0530

    birotlinesqview: adding Canvas extension methods

--- birotlinesqview/src/main/java/com/example/birotlinesqview/BiRotLineSqView.kt
+++ birotlinesqview/src/main/java/com/example/birotlinesqview/BiRotLineSqView.kt
@@ -24,8 +24,44 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 180f
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiRotLineSq(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(4)) {
+        rotate(deg * dsc(3))
+        for (j in 0..1) {
+            drawXY(-w * 0.5f * (1 - dsc(0)), -size + (size + size / 2) * j) {
+                drawRect(RectF(-size, 0f, 0f, size / 2), paint)
+            }
+            drawXY(0f, 0f) {
+                rotate(-rot * j * dsc(2))
+                drawLine(0f, 0f, 0f, -size * 0.5f * dsc(1), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBRLSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiRotLineSq(scale, w, h, paint)
+}
