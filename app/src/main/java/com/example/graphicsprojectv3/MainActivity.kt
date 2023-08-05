6d292c6c58fcebaa364f74db73836081225b84a6
Anwesh43 <anweshthecool0@gmail.com>
2023-08-05 12:01:14 +0530

    birotlinesqview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqflagrotview.SqFlagRotView
+import com.example.birotlinesqview.BiRotLineSqView
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SqFlagRotView.create(this)
+        BiRotLineSqView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
