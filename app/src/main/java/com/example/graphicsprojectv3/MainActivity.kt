97f08664f3bbe2b954ed92c25a46424e319b58da
Anwesh43 <anweshthecool0@gmail.com>
2023-08-06 12:34:56 +0530

    linerotsqleftdownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.birotlinesqview.BiRotLineSqView
-
+import com.example.linerotsqleftdownview.LineRotSqLeftDownView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BiRotLineSqView.create(this)
+        LineRotSqLeftDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
