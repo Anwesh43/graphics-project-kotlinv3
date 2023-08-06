561a4d9c3ea27474e3f177f061dc63d53e97c4b3
Anwesh43 <anweshthecool0@gmail.com>
2023-08-06 12:30:11 +0530

    linerotsqleftdownview: adding LineRotSqLeftDown class

--- linerotsqleftdownview/src/main/java/com/example/linerotsqleftdownview/LineRotSqLeftDownView.kt
+++ linerotsqleftdownview/src/main/java/com/example/linerotsqleftdownview/LineRotSqLeftDownView.kt
@@ -165,4 +165,27 @@ class LineRotSqLeftDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRoSqLeftDown(var i : Int) {
+
+        private var curr : LRSLDNode = LRSLDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
