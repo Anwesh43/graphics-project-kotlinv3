931d12b077e9802ceed294f3546076a9a687e22b
Anwesh43 <anweshthecool0@gmail.com>
2023-08-06 11:39:35 +0530

    linerotsqleftdownview: adding LineRotSqLeftDownView class

--- linerotsqleftdownview/src/main/java/com/example/linerotsqleftdownview/LineRotSqLeftDownView.kt
+++ linerotsqleftdownview/src/main/java/com/example/linerotsqleftdownview/LineRotSqLeftDownView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLRSLDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotSqLeftDown(scale, w, h, paint)
 }
+
+class LineRotSqLeftDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
