e164ee362a1055f3939506b107c0df16685dea87
Anwesh43 <anweshthecool0@gmail.com>
2023-08-06 12:36:22 +0530

    linerotsqleftdownview: adding fixes

--- linerotsqleftdownview/src/main/java/com/example/linerotsqleftdownview/LineRotSqLeftDownView.kt
+++ linerotsqleftdownview/src/main/java/com/example/linerotsqleftdownview/LineRotSqLeftDownView.kt
@@ -28,7 +28,7 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
-fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n))
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
 fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
     save()
