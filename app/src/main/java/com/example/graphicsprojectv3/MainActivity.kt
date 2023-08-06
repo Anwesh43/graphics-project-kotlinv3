bf70ff8e7d8b5c749a6f9b4b98f5a120ce11cb48
Anwesh43 <anweshthecool0@gmail.com>
2023-08-07 01:02:16 +0530

    linerotsemiarcview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotsqleftdownview.LineRotSqLeftDownView
+import com.example.linerotsemiarcview.LineRotSemiArcView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineRotSqLeftDownView.create(this)
+        LineRotSemiArcView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
