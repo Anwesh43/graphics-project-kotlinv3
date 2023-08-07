5e330dc21c9fe52978f8b65420d6121c2f608ea1
Anwesh43 <anweshthecool0@gmail.com>
2023-08-08 01:11:11 +0530

    barcaprotrightview: adding Renderer class

--- barcaprotrightview/src/main/java/com/example/barcaprotrightview/BarCapRotRightView.kt
+++ barcaprotrightview/src/main/java/com/example/barcaprotrightview/BarCapRotRightView.kt
@@ -186,4 +186,27 @@ class BarCapRotRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BarCapRotRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val bccr : BarCapRotRight = BarCapRotRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bccr.draw(canvas, paint)
+            animator.animate {
+                bccr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bccr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
