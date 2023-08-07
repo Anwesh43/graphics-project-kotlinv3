e29d5b3adc0c10f11f725ca5f77653e0930ab490
Anwesh43 <anweshthecool0@gmail.com>
2023-08-08 01:18:33 +0530

    barcaprotrightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotsemiarcview.LineRotSemiArcView
+import com.example.barcaprotrightview.BarCapRotRightView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineRotSemiArcView.create(this)
+        BarCapRotRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
