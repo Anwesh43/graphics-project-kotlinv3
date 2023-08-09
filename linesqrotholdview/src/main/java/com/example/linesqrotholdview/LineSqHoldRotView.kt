b9cc25035d6945550acb98313af2cd76c8dbe697
Anwesh43 <anweshthecool0@gmail.com>
2023-08-09 10:39:39 +0530

    linesqrotholdview: adding Canvas extension methods

--- linesqrotholdview/src/main/java/com/example/linesqrotholdview/LineSqHoldRotView.kt
+++ linesqrotholdview/src/main/java/com/example/linesqrotholdview/LineSqHoldRotView.kt
@@ -30,3 +30,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineSqHoldRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        drawXY((w / 2) * (1 - dsc(1)), 0f) {
+            rotate(rot * dsc(2))
+            drawRect(RectF(0f, 0f, size, size), paint)
+        }
+        drawXY(-w * 0.5f * (1 - dsc(0)), 0f) {
+            drawLine(0f, 0f, -size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLSHRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSqHoldRot(scale, w, h, paint)
+}
