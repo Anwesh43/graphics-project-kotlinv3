81dffba5e12c02334528245885dde210a22dc05c
Anwesh43 <anweshthecool0@gmail.com>
2023-08-09 11:32:51 +0530

    linesqrotholdview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.barcaprotrightview.BarCapRotRightView
+import com.example.linesqrotholdview.LineSqHoldRotView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BarCapRotRightView.create(this)
+        LineSqHoldRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
