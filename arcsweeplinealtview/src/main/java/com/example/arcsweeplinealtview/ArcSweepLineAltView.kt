94a85ce0bba4c10ccaa98087f91bf0cc6a149f71
Anwesh43 <anweshthecool0@gmail.com>
2023-08-10 12:46:54 +0530

    arcsweeplinealtview: adding ArcSweepLineAltView class

--- arcsweeplinealtview/src/main/java/com/example/arcsweeplinealtview/ArcSweepLineAltView.kt
+++ arcsweeplinealtview/src/main/java/com/example/arcsweeplinealtview/ArcSweepLineAltView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawASLANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcSweepLineAlt(scale, w, h, paint)
 }
+
+class ArcSweepLineAltView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
