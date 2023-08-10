d019201e204ed40346f90ac66b37ad778a869e98
Anwesh43 <anweshthecool0@gmail.com>
2023-08-10 13:05:57 +0530

    arcsweeplinealtview: adding ASLANode class

--- arcsweeplinealtview/src/main/java/com/example/arcsweeplinealtview/ArcSweepLineAltView.kt
+++ arcsweeplinealtview/src/main/java/com/example/arcsweeplinealtview/ArcSweepLineAltView.kt
@@ -124,4 +124,45 @@ class ArcSweepLineAltView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ASLANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ASLANode? = null
+        private var prev : ASLANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ASLANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawASLANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ASLANode {
+            var curr : ASLANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
