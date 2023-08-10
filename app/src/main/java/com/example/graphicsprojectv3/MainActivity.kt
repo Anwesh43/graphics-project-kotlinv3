12759289c575c8f5e385ba794ae24def236ac954
Anwesh43 <anweshthecool0@gmail.com>
2023-08-10 13:22:27 +0530

    arcsweeplinealtview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linesqrotholdview.LineSqHoldRotView
+import com.example.arcsweeplinealtview.ArcSweepLineAltView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineSqHoldRotView.create(this)
+        ArcSweepLineAltView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
