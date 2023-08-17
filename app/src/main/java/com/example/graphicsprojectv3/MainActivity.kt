5a48cbb5f107977d6f5b4b731737a5e344c3b88b
Anwesh43 <anweshthecool0@gmail.com>
2023-08-17 14:45:00 +0530

    linerotaltupview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.arcsweeplinealtview.ArcSweepLineAltView
+import com.example.linerotaltupview.LineRotAltUpView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        ArcSweepLineAltView.create(this)
+        LineRotAltUpView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
