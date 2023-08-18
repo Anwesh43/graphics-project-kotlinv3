5251b452fa383eeba25149f6bb229cfe51693416
Anwesh43 <anweshthecool0@gmail.com>
2023-08-18 11:47:55 +0530

    biblockrotleftview: adding Renderer in View

--- biblockrotleftview/src/main/java/com/example/biblockrotleftview/BiBlockRotLeftView.kt
+++ biblockrotleftview/src/main/java/com/example/biblockrotleftview/BiBlockRotLeftView.kt
@@ -64,14 +64,16 @@ fun Canvas.drawBBRLNode(i : Int, scale : Float, paint : Paint) {
 
 class BiBlockRotLeftView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
