e141d4722a77c16619faa41b54358f0adc59a7cc
Anwesh43 <anweshthecool0@gmail.com>
2023-08-18 11:55:56 +0530

    biblockrotleftview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotaltupview.LineRotAltUpView
+import com.example.biblockrotleftview.BiBlockRotLeftView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineRotAltUpView.create(this)
+        BiBlockRotLeftView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
