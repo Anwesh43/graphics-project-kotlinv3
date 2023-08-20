187abde422f4bf1bf965a049bef3e8e8211690da
Anwesh43 <anweshthecool0@gmail.com>
2023-08-20 10:21:21 +0530

    linedroprotarcview: adding LDRANode class

--- linedroprotarcview/src/main/java/com/example/linedroprotarcview/LineDropRotArcView.kt
+++ linedroprotarcview/src/main/java/com/example/linedroprotarcview/LineDropRotArcView.kt
@@ -123,4 +123,45 @@ class LineDropRotArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDRANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDRANode? = null
+        private var prev : LDRANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDRANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDRANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDRANode {
+            var curr : LDRANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
