800411d15dae85263972fdaacf78e5a42c39ecb6
Anwesh43 <anweshthecool0@gmail.com>
2023-08-22 19:05:28 +0530

    biarcrotjoindownview: adding BiArcRotJoinDownView class

--- biarcrotjoindownview/src/main/java/com/example/biarcrotjoindownview/BiArcRotJoinDownView.kt
+++ biarcrotjoindownview/src/main/java/com/example/biarcrotjoindownview/BiArcRotJoinDownView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBARJDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBiArcJoinDown(scale, w, h, paint)
 }
+
+class BiArcRotJoinDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
