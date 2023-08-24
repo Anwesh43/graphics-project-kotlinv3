cc5e4ecb31ce3ea044e78e25e7377a8be17d77b1
Anwesh43 <anweshthecool0@gmail.com>
2023-08-24 07:30:23 +0530

    biarcrotjoindownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linedroprotarcview.LineDropRotArcView
+import com.example.biarcrotjoindownview.BiArcRotJoinDownView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineDropRotArcView.create(this)
+        BiArcRotJoinDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
