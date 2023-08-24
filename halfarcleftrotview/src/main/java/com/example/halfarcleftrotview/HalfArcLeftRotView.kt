aff6c176ff238927ae557724074a443cd6fac770
Anwesh43 <anweshthecool0@gmail.com>
2023-08-25 00:41:42 +0530

    halfarcleftrotview: adding HalfArcLeftRotView class

--- halfarcleftrotview/src/main/java/com/example/halfarcleftrotview/HalfArcLeftRotView.kt
+++ halfarcleftrotview/src/main/java/com/example/halfarcleftrotview/HalfArcLeftRotView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawHALRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHalfArcLeftRot(scale, w, h, paint)
 }
+
+class HalfArcLeftRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
