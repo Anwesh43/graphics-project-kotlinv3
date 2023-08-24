857ca377fa7688698ad7d5b3782c1470b592bf8b
Anwesh43 <anweshthecool0@gmail.com>
2023-08-25 01:14:41 +0530

    halfarcleftrotview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.biarcrotjoindownview.BiArcRotJoinDownView
+import com.example.halfarcleftrotview.HalfArcLeftRotView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BiArcRotJoinDownView.create(this)
+        HalfArcLeftRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
