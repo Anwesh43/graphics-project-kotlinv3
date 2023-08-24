76299c6d0997ba2f7ec2428a07ebca1cced2c661
Anwesh43 <anweshthecool0@gmail.com>
2023-08-25 01:03:21 +0530

    halfarcleftrotview: adding HALRNode class

--- halfarcleftrotview/src/main/java/com/example/halfarcleftrotview/HalfArcLeftRotView.kt
+++ halfarcleftrotview/src/main/java/com/example/halfarcleftrotview/HalfArcLeftRotView.kt
@@ -122,4 +122,44 @@ class HalfArcLeftRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HALRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HALRNode? = null
+        private var prev : HALRNode? = null
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HALRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHALRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HALRNode {
+            var curr : HALRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
