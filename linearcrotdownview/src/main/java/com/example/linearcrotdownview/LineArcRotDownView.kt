4fcce418f79c4391de26494bb1e438f7ea151e8e
Anwesh43 <anweshthecool0@gmail.com>
2023-08-26 13:10:46 +0530

    linearcrotdownview: adding Renderer in View

--- linearcrotdownview/src/main/java/com/example/linearcrotdownview/LineArcRotDownView.kt
+++ linearcrotdownview/src/main/java/com/example/linearcrotdownview/LineArcRotDownView.kt
@@ -62,14 +62,16 @@ fun Canvas.drawLARDNode(i : Int, scale : Float, paint : Paint) {
 
 class LineArcRotDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -129,7 +131,7 @@ class LineArcRotDownView(ctx : Context) : View(ctx) {
         private var prev : LARDNode? = null
 
         init {
-
+            addNeighbor()
         }
 
         fun addNeighbor() {
