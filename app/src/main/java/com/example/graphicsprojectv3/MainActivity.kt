98a7f73303c369efd36afab7846594a7a8bc8107
Anwesh43 <anweshthecool0@gmail.com>
2023-08-26 13:12:27 +0530

    linearcrotdownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.halfarcleftrotview.HalfArcLeftRotView
+import com.example.linearcrotdownview.LineArcRotDownView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        HalfArcLeftRotView.create(this)
+        LineArcRotDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
