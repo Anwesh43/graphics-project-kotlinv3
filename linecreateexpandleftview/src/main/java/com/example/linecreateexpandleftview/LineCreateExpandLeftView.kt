a71301380e5a5cbf02f7f0e943c7450409c8b191
Anwesh43 <anweshthecool0@gmail.com>
2023-08-27 09:28:50 +0530

    linecreateexpandleftview: adding constants

---
+++ linecreateexpandleftview/src/main/java/com/example/linecreateexpandleftview/LineCreateExpandLeftView.kt
@@ -0,0 +1,27 @@
+package com.example.linecreateexpandleftview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 45f
