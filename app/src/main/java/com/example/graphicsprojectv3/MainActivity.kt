e347b22149372cddd6e690e7d2ec2a4a5ce41e07
Anwesh43 <anweshthecool0@gmail.com>
2023-08-29 20:13:27 +0530

    linebreakaltview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linecreateexpandleftview.LineCreateExpandLeftView
+import com.example.linebreakaltview.LineBreakAltView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineCreateExpandLeftView.create(this)
+        LineBreakAltView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
