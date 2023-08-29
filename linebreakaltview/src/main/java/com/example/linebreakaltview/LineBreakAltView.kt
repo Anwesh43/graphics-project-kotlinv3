7b165fa34a238aea651046e3778097696f7eaf90
Anwesh43 <anweshthecool0@gmail.com>
2023-08-29 19:43:59 +0530

    linebreakaltview: adding LineBreakAltView class

--- linebreakaltview/src/main/java/com/example/linebreakaltview/LineBreakAltView.kt
+++ linebreakaltview/src/main/java/com/example/linebreakaltview/LineBreakAltView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLBANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBreakAlt(scale, w, h, paint)
 }
+
+class LineBreakAltView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
