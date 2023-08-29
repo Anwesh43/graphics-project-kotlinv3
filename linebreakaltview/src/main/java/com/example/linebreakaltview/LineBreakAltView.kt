60e090eb506456aa1677b443a98fa37e59376b34
Anwesh43 <anweshthecool0@gmail.com>
2023-08-29 19:49:17 +0530

    linebreakaltview: adding LBANode class

--- linebreakaltview/src/main/java/com/example/linebreakaltview/LineBreakAltView.kt
+++ linebreakaltview/src/main/java/com/example/linebreakaltview/LineBreakAltView.kt
@@ -123,4 +123,45 @@ class LineBreakAltView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBANode? = null
+        private var prev : LBANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBANode {
+            var curr : LBANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
