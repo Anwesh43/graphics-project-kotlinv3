28e81ca99f38247eab961b65d5c50b42e6590cc8
Anwesh43 <anweshthecool0@gmail.com>
2023-09-01 16:40:45 +0530

    halfarclinejoinview: adding HALJNode class

--- halfarclinejoinview/src/main/java/com/example/halfarclinejoinview/HalfArcLineJoinView.kt
+++ halfarclinejoinview/src/main/java/com/example/halfarclinejoinview/HalfArcLineJoinView.kt
@@ -133,4 +133,45 @@ class HalfArcLineJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HALJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HALJNode? = null
+        private var prev : HALJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HALJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHALJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HALJNode {
+            var curr : HALJNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
