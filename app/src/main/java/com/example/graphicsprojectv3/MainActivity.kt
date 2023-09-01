1c7f15294915d1aa871dd34f764d22071e506bc4
Anwesh43 <anweshthecool0@gmail.com>
2023-09-01 18:00:33 +0530

    halfarclinejoinview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linebreakaltview.LineBreakAltView
+import com.example.halfarclinejoinview.HalfArcLineJoinView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineBreakAltView.create(this)
+        HalfArcLineJoinView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
