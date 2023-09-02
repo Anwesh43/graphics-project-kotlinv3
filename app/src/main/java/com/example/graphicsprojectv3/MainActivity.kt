52c9bd85e4e886d788794ff2c618d8218ba78008
Anwesh43 <anweshthecool0@gmail.com>
2023-09-02 11:07:35 +0530

    semiarcblockrotview: ading view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.halfarclinejoinview.HalfArcLineJoinView
+import com.example.semiarcblockrotview.SemiArcBlockRotView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        HalfArcLineJoinView.create(this)
+        SemiArcBlockRotView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
