583ee0452a84902dd633138ac4a67020a338ddd8
Anwesh43 <anweshthecool0@gmail.com>
2023-09-06 11:27:53 +0530

    lineslanthorizview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.semiarcblockrotview.SemiArcBlockRotView
+import com.example.lineslanthorizview.LineSlantHorizView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        SemiArcBlockRotView.create(this)
+        LineSlantHorizView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
