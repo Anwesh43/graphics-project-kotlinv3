fc9ae7c7837e311f178e4b324e9efd30299e02b6
Anwesh43 <anweshthecool0@gmail.com>
2023-09-07 12:03:23 +0530

    bijoinlineupview: adding BJLUNode class

--- bijoinlineupview/src/main/java/com/example/bijoinlineupview/BiJoinLineUpView.kt
+++ bijoinlineupview/src/main/java/com/example/bijoinlineupview/BiJoinLineUpView.kt
@@ -125,4 +125,45 @@ class BiJoinLineUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BJLUNode(var i : Int, val state : State = State()) {
+
+        private var next : BJLUNode? = null
+        private var prev : BJLUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BJLUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBJLUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BJLUNode {
+            var curr : BJLUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
