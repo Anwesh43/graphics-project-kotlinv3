a245b9d95dc3ff4e4d3f363b2f639657b757d1c3
Anwesh43 <anweshthecool0@gmail.com>
2023-09-07 12:40:42 +0530

    bijoinlineupview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineslanthorizview.LineSlantHorizView
+import com.example.bijoinlineupview.BiJoinLineUpView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineSlantHorizView.create(this)
+        BiJoinLineUpView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
