7efec3be241a5f045cc6df1c34918968f369ec2c
Anwesh43 <anweshthecool0@gmail.com>
2023-09-07 11:52:09 +0530

    bijoinlineupview: adding BiJoinLineUpView class

--- bijoinlineupview/src/main/java/com/example/bijoinlineupview/BiJoinLineUpView.kt
+++ bijoinlineupview/src/main/java/com/example/bijoinlineupview/BiJoinLineUpView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBJLUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiJoinLineUp(scale, w, h, paint)
 }
+
+class BiJoinLineUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
