b2584996d8d3bd01b939a451ed07bae4bd10084b
Anwesh43 <anweshthecool0@gmail.com>
2023-09-07 11:16:25 +0530

    bijoinlineupview: adding Int and Float extension methods

--- bijoinlineupview/src/main/java/com/example/bijoinlineupview/BiJoinLineUpView.kt
+++ bijoinlineupview/src/main/java/com/example/bijoinlineupview/BiJoinLineUpView.kt
@@ -25,3 +25,7 @@ val rot : Float = 45f
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
