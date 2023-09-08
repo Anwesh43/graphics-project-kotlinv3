65366dfc137dbd5c5f8e2cbbbae231b6aaad2eaa
Anwesh43 <anweshthecool0@gmail.com>
2023-09-08 13:29:58 +0530

    tlineblockview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.bijoinlineupview.BiJoinLineUpView
+import com.example.tlineblockview.TLineBlockView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BiJoinLineUpView.create(this)
+        TLineBlockView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
