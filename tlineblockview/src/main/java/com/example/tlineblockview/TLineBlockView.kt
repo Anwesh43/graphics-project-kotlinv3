0cd2d6320ee5fa20279d68fd0a6ee2a9bb6a782c
Anwesh43 <anweshthecool0@gmail.com>
2023-09-08 12:29:02 +0530

    tlineblockview: adding TLBNode class

--- tlineblockview/src/main/java/com/example/tlineblockview/TLineBlockView.kt
+++ tlineblockview/src/main/java/com/example/tlineblockview/TLineBlockView.kt
@@ -124,4 +124,45 @@ class TLineBlockView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TLBNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : TLBNode? = null
+        private var prev : TLBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TLBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTLBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TLBNode {
+            var curr : TLBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
