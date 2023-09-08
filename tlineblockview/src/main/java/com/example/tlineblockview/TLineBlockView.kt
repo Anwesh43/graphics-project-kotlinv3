b6a768be01a031636f43741e905fd2e3f8c179ee
Anwesh43 <anweshthecool0@gmail.com>
2023-09-08 12:23:37 +0530

    tlineblockview: adding TLineBlockView class

--- tlineblockview/src/main/java/com/example/tlineblockview/TLineBlockView.kt
+++ tlineblockview/src/main/java/com/example/tlineblockview/TLineBlockView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawTLBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTLineBlock(scale, w, h, paint)
 }
+
+class TLineBlockView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
