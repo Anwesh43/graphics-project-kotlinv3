92a028519f6b1aeabcbe3a7b335f72ab595bcfa1
Anwesh43 <anweshthecool0@gmail.com>
2023-09-10 14:16:36 +0530

    halfarcbilinerightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.tlineblockview.TLineBlockView
+import com.example.halfarcbilinerightview.HalfArcBiLineRightView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        TLineBlockView.create(this)
+        HalfArcBiLineRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
