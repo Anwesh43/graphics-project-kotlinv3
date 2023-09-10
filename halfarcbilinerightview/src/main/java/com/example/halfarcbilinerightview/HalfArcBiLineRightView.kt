f4ed0e8e585bb97d54cc0689215c669b1b469cbe
Anwesh43 <anweshthecool0@gmail.com>
2023-09-10 13:54:49 +0530

    halfarcbilinerightview: adding HalfArcBiLineRightView class

--- halfarcbilinerightview/src/main/java/com/example/halfarcbilinerightview/HalfArcBiLineRightView.kt
+++ halfarcbilinerightview/src/main/java/com/example/halfarcbilinerightview/HalfArcBiLineRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawHABLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHalfArcBiLineRight(scale, w, h, paint)
 }
+
+class HalfArcBiLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
