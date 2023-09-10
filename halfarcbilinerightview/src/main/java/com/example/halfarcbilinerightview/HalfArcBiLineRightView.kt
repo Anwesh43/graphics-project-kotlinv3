210d6782a52570637c04fd0cd018ebd9e1c3b61e
Anwesh43 <anweshthecool0@gmail.com>
2023-09-10 14:12:00 +0530

    halfarcbilinerightview: adding HABLRNode class

--- halfarcbilinerightview/src/main/java/com/example/halfarcbilinerightview/HalfArcBiLineRightView.kt
+++ halfarcbilinerightview/src/main/java/com/example/halfarcbilinerightview/HalfArcBiLineRightView.kt
@@ -125,4 +125,45 @@ class HalfArcBiLineRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HABLRNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : HABLRNode? = null
+        private var next : HABLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HABLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHABLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HABLRNode {
+            var curr : HABLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
