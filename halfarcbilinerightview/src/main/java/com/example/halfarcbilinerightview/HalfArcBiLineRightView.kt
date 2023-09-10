6771f47b5ffe967b95fbf10912ced685341fdbf1
Anwesh43 <anweshthecool0@gmail.com>
2023-09-10 14:13:21 +0530

    halfarcbilinerightview: adding HalfArcBiLineRight class

--- halfarcbilinerightview/src/main/java/com/example/halfarcbilinerightview/HalfArcBiLineRightView.kt
+++ halfarcbilinerightview/src/main/java/com/example/halfarcbilinerightview/HalfArcBiLineRightView.kt
@@ -166,4 +166,27 @@ class HalfArcBiLineRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class HalfArcBiLineRight(var i : Int = 0) {
+
+        private var curr : HABLRNode = HABLRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
