b956006c294cc0604630fbfaa5b60f4c01594fe3
Anwesh43 <anweshthecool0@gmail.com>
2023-09-11 20:51:47 +0530

    llinerotleftview: adding Canvas extension methods

--- llinerotleftview/src/main/java/com/example/llinerotleftview/LLineRotLeftView.kt
+++ llinerotleftview/src/main/java/com/example/llinerotleftview/LLineRotLeftView.kt
@@ -29,3 +29,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLLineRotLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        drawXY(w * 0.5f * (1 - dsc(0)), size) {
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLLRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLLineRotLeft(scale, w, h, paint)
+}
