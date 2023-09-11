fe55beba7a50f039a000e90daeae3fb5c26a5f6f
Anwesh43 <anweshthecool0@gmail.com>
2023-09-11 21:12:46 +0530

    llinerotleftview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.halfarcbilinerightview.HalfArcBiLineRightView
+import com.example.llinerotleftview.LLineRotLeftView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        HalfArcBiLineRightView.create(this)
+        LLineRotLeftView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
