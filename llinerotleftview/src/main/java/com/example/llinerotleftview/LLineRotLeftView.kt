0f7c7b2456220f81cc0dc4b4dfdff774d824f512
Anwesh43 <anweshthecool0@gmail.com>
2023-09-11 20:52:54 +0530

    llinerotleftview: adding LLineRotLeftView class

--- llinerotleftview/src/main/java/com/example/llinerotleftview/LLineRotLeftView.kt
+++ llinerotleftview/src/main/java/com/example/llinerotleftview/LLineRotLeftView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLLRLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLLineRotLeft(scale, w, h, paint)
 }
+
+class LLineRotLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
