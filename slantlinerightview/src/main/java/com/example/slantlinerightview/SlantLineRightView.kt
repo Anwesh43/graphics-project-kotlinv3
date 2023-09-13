3f66edaa9eb2d28716caee8bb6cf9b344e09dda1
Anwesh43 <anweshthecool0@gmail.com>
2023-09-13 10:37:11 +0530

    slantlinerightview: adding SlantLineRightView class

--- slantlinerightview/src/main/java/com/example/slantlinerightview/SlantLineRightView.kt
+++ slantlinerightview/src/main/java/com/example/slantlinerightview/SlantLineRightView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawSLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSlantLineRight(scale, w, h, paint)
 }
+
+class SlantLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
