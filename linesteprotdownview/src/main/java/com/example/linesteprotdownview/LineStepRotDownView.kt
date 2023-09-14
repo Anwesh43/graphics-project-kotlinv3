3df304a6e445af563a26e92e95d7754db2795525
Anwesh43 <anweshthecool0@gmail.com>
2023-09-14 13:48:23 +0530

    linesteprotdownview: adding LSRDNode class

--- linesteprotdownview/src/main/java/com/example/linesteprotdownview/LineStepRotDownView.kt
+++ linesteprotdownview/src/main/java/com/example/linesteprotdownview/LineStepRotDownView.kt
@@ -123,4 +123,45 @@ class LineStepRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSRDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LSRDNode? = null
+        private var prev : LSRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSRDNode {
+            var curr : LSRDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
