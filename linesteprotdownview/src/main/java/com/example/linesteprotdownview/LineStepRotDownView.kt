1ef92d0bdd4f1ebfedade86a2db56587bc6e70fd
Anwesh43 <anweshthecool0@gmail.com>
2023-09-14 13:37:51 +0530

    linesteprotdownview: adding Canvas extension methods

--- linesteprotdownview/src/main/java/com/example/linesteprotdownview/LineStepRotDownView.kt
+++ linesteprotdownview/src/main/java/com/example/linesteprotdownview/LineStepRotDownView.kt
@@ -28,3 +28,35 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineStepRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        for (j in 0..2) {
+            drawXY(0f, -size * 0.5f + size * 0.5f * j) {
+                val dsize : Float = size * 0.5f * (1 + (1 - (j % 2)))
+                drawLine(0f, 0f, -dsize * dsc(j), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLSRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineStepRotDown(scale, w, h, paint)
+}
