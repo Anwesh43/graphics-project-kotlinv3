7122144243079e0c70df8daf778d78dedfe71436
Anwesh43 <anweshthecool0@gmail.com>
2023-09-15 12:23:15 +0530

    blockbilineupview: adding BBLUNode class

--- blockbilineupview/src/main/java/com/example/blockbilineupview/BlockBiLineUpView.kt
+++ blockbilineupview/src/main/java/com/example/blockbilineupview/BlockBiLineUpView.kt
@@ -123,4 +123,45 @@ class BlockBiLineUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BBLUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BBLUNode? = null
+        private var prev : BBLUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BBLUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBLUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBLUNode {
+            var curr : BBLUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
