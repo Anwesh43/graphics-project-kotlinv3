7c5b8f3d09abed3be9197e7f3b1c2e65157b4508
Anwesh43 <anweshthecool0@gmail.com>
2023-09-15 12:09:38 +0530

    blockbilineupview: adding BlockBiLineUpView class

--- blockbilineupview/src/main/java/com/example/blockbilineupview/BlockBiLineUpView.kt
+++ blockbilineupview/src/main/java/com/example/blockbilineupview/BlockBiLineUpView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBBLUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBlockBiLineUp(scale, w, h, paint)
 }
+
+class BlockBiLineUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
