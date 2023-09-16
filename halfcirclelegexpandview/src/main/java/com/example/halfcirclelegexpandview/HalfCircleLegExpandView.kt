f098fe07cb546e6259c734e1fd4598ae37aa6864
Anwesh43 <anweshthecool0@gmail.com>
2023-09-16 12:49:03 +0530

    halfcirclelegexpandview: adding HalfCircleLegExpandView class

--- halfcirclelegexpandview/src/main/java/com/example/halfcirclelegexpandview/HalfCircleLegExpandView.kt
+++ halfcirclelegexpandview/src/main/java/com/example/halfcirclelegexpandview/HalfCircleLegExpandView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawHCLENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHalfCircleLegExpand(scale, w, h, paint)
 }
+
+class HalfCircleLegExpandView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
