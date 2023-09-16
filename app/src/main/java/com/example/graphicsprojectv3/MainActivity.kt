a0239b6ce044ca7f853d6788babe2638138a6e5f
Anwesh43 <anweshthecool0@gmail.com>
2023-09-16 13:13:18 +0530

    halfcirclelegexpandview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.blockbilineupview.BlockBiLineUpView
+import com.example.halfcirclelegexpandview.HalfCircleLegExpandView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BlockBiLineUpView.create(this)
+        HalfCircleLegExpandView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
