e1df3e21947d44f6b4164d33bf2a5bb20fc0123e
Anwesh43 <anweshthecool0@gmail.com>
2023-09-16 12:55:02 +0530

    halfcirclelegexpandview: adding HCLENode class

--- halfcirclelegexpandview/src/main/java/com/example/halfcirclelegexpandview/HalfCircleLegExpandView.kt
+++ halfcirclelegexpandview/src/main/java/com/example/halfcirclelegexpandview/HalfCircleLegExpandView.kt
@@ -124,4 +124,45 @@ class HalfCircleLegExpandView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HCLENode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HCLENode? = null
+        private var prev : HCLENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HCLENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHCLENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HCLENode {
+            var curr : HCLENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
