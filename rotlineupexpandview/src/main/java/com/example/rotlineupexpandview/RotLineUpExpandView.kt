14763d03f09052e25ba4ea92b9d26aa255d236fe
Anwesh43 <anweshthecool0@gmail.com>
2023-09-17 08:36:44 +0530

    rotlineupexpandview: adding RotLineUpExpand class

--- rotlineupexpandview/src/main/java/com/example/rotlineupexpandview/RotLineUpExpandView.kt
+++ rotlineupexpandview/src/main/java/com/example/rotlineupexpandview/RotLineUpExpandView.kt
@@ -147,7 +147,7 @@ class RotLineUpExpandView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
-        fun startUdpating(cb : () -> Unit) {
+        fun startUpdating(cb : () -> Unit) {
             state.startUpdating(cb)
         }
 
@@ -161,6 +161,29 @@ class RotLineUpExpandView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class RotLineUpExpand(var i : Int) {
+
+        private var curr : RLUENode = RLUENode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
