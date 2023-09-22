287e497992bb39acf49153a5d57769631d02f649
Anwesh43 <anweshthecool0@gmail.com>
2023-09-22 12:19:31 +0530

    halfarcrectjoinview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.halfarcdownlineview.HalfArcDownLineView
+import com.example.halfarcrectjoinview.HalfArcRectJoinView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        HalfArcDownLineView.create(this)
+        HalfArcRectJoinView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
