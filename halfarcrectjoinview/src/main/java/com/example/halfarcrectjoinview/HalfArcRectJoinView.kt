f7ae719fcbc8e334c84dfdccead75bc40917e0be
Anwesh43 <anweshthecool0@gmail.com>
2023-09-22 12:20:11 +0530

    halfarcrectjoinview: adding fixes

--- halfarcrectjoinview/src/main/java/com/example/halfarcrectjoinview/HalfArcRectJoinView.kt
+++ halfarcrectjoinview/src/main/java/com/example/halfarcrectjoinview/HalfArcRectJoinView.kt
@@ -42,7 +42,7 @@ fun Canvas.drawHalfArcRectJoin(scale : Float, w : Float, h : Float, paint : Pain
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
         drawXY(0f, 0f) {
             rotate(rot * dsc(2))
             drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(1), true, paint)
