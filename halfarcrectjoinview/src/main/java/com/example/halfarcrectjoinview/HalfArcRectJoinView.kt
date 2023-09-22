772de74fc93a3298dad45ce60a0943abfdcec3f7
Anwesh43 <anweshthecool0@gmail.com>
2023-09-22 11:58:12 +0530

    halfarcrectjoinview: adding HalfarcRectJoin clas

--- halfarcrectjoinview/src/main/java/com/example/halfarcrectjoinview/HalfArcRectJoinView.kt
+++ halfarcrectjoinview/src/main/java/com/example/halfarcrectjoinview/HalfArcRectJoinView.kt
@@ -161,4 +161,27 @@ class HalfArcRectJoinView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class HalfArcRectJoin(var i : Int) {
+
+        private var curr : HARNode = HARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdaitng(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
