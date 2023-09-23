24b8ca9a31dde76bc68ae5fbe5a4c8299631dc14
Anwesh43 <anweshthecool0@gmail.com>
2023-09-23 13:07:39 +0530

    biarcrotdownview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.halfarcrectjoinview.HalfArcRectJoinView
+import com.example.biarcrotdownview.BiArcRotDownView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        HalfArcRectJoinView.create(this)
+        BiArcRotDownView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
