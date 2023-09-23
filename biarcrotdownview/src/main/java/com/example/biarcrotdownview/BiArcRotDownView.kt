868b2c166fd8783d2e678a4018d1088f9ea1476e
Anwesh43 <anweshthecool0@gmail.com>
2023-09-23 12:53:42 +0530

    biarcrotdownview: adding Canvas  extension methods

--- biarcrotdownview/src/main/java/com/example/biarcrotdownview/BiArcRotDownView.kt
+++ biarcrotdownview/src/main/java/com/example/biarcrotdownview/BiArcRotDownView.kt
@@ -29,3 +29,38 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiArcRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size * 1.5f) * dsc(3)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * j * dsc(2))
+                scale(1f, 1f - 2 * j)
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+                drawXY(0f, -size) {
+                    drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(1), true, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBARDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiArcRotDown(scale, w, h, paint)
+}
