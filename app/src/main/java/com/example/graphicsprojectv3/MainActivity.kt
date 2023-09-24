f68d6a4fc9b874b6a0edcebaa02bff4a81bbf5ec
Anwesh43 <anweshthecool0@gmail.com>
2023-09-24 13:51:38 +0530

    linetoyrotupview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.biarcrotdownview.BiArcRotDownView
+import com.example.linetoyrotupview.LineToYRotUpView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BiArcRotDownView.create(this)
+        LineToYRotUpView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
