2c8bda53e937cc61620a5d173123dfe4ab291c62
Anwesh43 <anweshthecool0@gmail.com>
2023-09-24 13:39:04 +0530

    linetoyrotupview: adding LTYRUNode class

--- linetoyrotupview/src/main/java/com/example/linetoyrotupview/LineToYRotUpView.kt
+++ linetoyrotupview/src/main/java/com/example/linetoyrotupview/LineToYRotUpView.kt
@@ -124,4 +124,45 @@ class LineToYRotUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTYRUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LTYRUNode? = null
+        private var prev : LTYRUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTYRUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTYRUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTYRUNode {
+            var curr : LTYRUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
