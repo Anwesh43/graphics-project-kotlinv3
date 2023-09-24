4388378448d8b596f7f5f9a41da1fa71a1ea5a8e
Anwesh43 <anweshthecool0@gmail.com>
2023-09-24 13:24:57 +0530

    linetoyrotupview: adding Canvas extension methods

--- linetoyrotupview/src/main/java/com/example/linetoyrotupview/LineToYRotUpView.kt
+++ linetoyrotupview/src/main/java/com/example/linetoyrotupview/LineToYRotUpView.kt
@@ -29,3 +29,35 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineToYRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2 + size) * dsc(3)) {
+        rotate(deg * dsc(2))
+        for (j in 0..2) {
+            drawXY(0f, 0f) {
+                rotate(rot * (j - 1) * dsc(1))
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLTYRUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineToYRotUp(scale, w, h, paint)
+}
