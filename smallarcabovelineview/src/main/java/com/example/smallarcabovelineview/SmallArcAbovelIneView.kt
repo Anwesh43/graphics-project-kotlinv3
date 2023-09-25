30ae6c44fdb85e87c284f17bc616329c2e87ae85
Anwesh43 <anweshthecool0@gmail.com>
2023-09-25 10:24:31 +0530

    smallarcabovelineview: adding SAALNode class

--- smallarcabovelineview/src/main/java/com/example/smallarcabovelineview/SmallArcAbovelIneView.kt
+++ smallarcabovelineview/src/main/java/com/example/smallarcabovelineview/SmallArcAbovelIneView.kt
@@ -121,4 +121,45 @@ class SmallArcAboveLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SAALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SAALNode? = null
+        private var prev : SAALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SAALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSAALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SAALNode {
+            var curr : SAALNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
