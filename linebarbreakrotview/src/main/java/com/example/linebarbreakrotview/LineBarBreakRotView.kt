63e5d5b57dcf1c41113f1090fb2d9a12ef7f5bf8
Anwesh43 <anweshthecool0@gmail.com>
2023-10-01 23:33:46 +0530

    linebarbreakrotview: adding Canvas extension methods

--- linebarbreakrotview/src/main/java/com/example/linebarbreakrotview/LineBarBreakRotView.kt
+++ linebarbreakrotview/src/main/java/com/example/linebarbreakrotview/LineBarBreakRotView.kt
@@ -30,3 +30,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBarBreakRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h/ 2) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        drawRect(RectF(0f, 0f, size / 4, size * dsc(1)), paint)
+    }
+}
+
+fun Canvas.drawLBBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBarBreakRot(scale, w, h, paint)
+}
