e81e49b8bda4739691624a6d519891630193c14f
Anwesh43 <anweshthecool0@gmail.com>
2023-10-03 10:42:52 +0530

    linerotsqdownview: adding LineRotSqDown class

--- linerotsqdownview/src/main/java/com/example/linerotsqdownview/LineRotSqDownView.kt
+++ linerotsqdownview/src/main/java/com/example/linerotsqdownview/LineRotSqDownView.kt
@@ -166,4 +166,27 @@ class LineRotSqDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotSqDown(var i : Int = 0, val state : State = State()) {
+
+        private var curr : LRSDNode = LRSDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
