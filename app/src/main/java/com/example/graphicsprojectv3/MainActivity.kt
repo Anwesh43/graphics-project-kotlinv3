52505ab1894db2239af9247c731530559b57f36f
Anwesh43 <anweshthecool0@gmail.com>
2023-10-04 16:24:06 +0530

    rightlinerotrightview: addingview in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotsqdownview.LineRotSqDownView
+import com.example.rightlinerotrightview.RightLineRotRightView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        LineRotSqDownView.create(this)
+        RightLineRotRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
