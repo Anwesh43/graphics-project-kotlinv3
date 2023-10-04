58bae32cbffce997c9b5869d9ff6cbed8dd17335
Anwesh43 <anweshthecool0@gmail.com>
2023-10-04 16:15:18 +0530

    rightlinerotrightview: adding RLRRNode class

--- rightlinerotrightview/src/main/java/com/example/rightlinerotrightview/RightLineRotRightView.kt
+++ rightlinerotrightview/src/main/java/com/example/rightlinerotrightview/RightLineRotRightView.kt
@@ -124,4 +124,45 @@ class RightLineRotRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RLRRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RLRRNode? = null
+        private var prev : RLRRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RLRRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRLRRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RLRRNode {
+            var curr : RLRRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
