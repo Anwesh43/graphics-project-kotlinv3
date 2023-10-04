2e46d2b3f5e4818ce2e12ae3df485d235de3de8c
Anwesh43 <anweshthecool0@gmail.com>
2023-10-04 15:58:44 +0530

    rightlinerotrightview: adding RightLineRotRightView class

--- rightlinerotrightview/src/main/java/com/example/rightlinerotrightview/RightLineRotRightView.kt
+++ rightlinerotrightview/src/main/java/com/example/rightlinerotrightview/RightLineRotRightView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawRLRRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRightLineRotRight(scale, w, h, paint)
 }
+
+class RightLineRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
