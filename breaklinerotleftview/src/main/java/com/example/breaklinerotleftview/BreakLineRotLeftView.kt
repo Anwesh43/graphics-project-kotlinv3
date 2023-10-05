9bb7a3c0a4f19ad4af70c29b78dc478b8d219f69
Anwesh43 <anweshthecool0@gmail.com>
2023-10-05 09:47:00 +0530

    breaklinerotleftview: adding Canvas extension methods

--- breaklinerotleftview/src/main/java/com/example/breaklinerotleftview/BreakLineRotLeftView.kt
+++ breaklinerotleftview/src/main/java/com/example/breaklinerotleftview/BreakLineRotLeftView.kt
@@ -22,9 +22,40 @@ val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
-val rot : Float = 90f
+val rot : Float = 30f
 val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBreakLineRotLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * j * dsc(1) - (180 + rot) * (1 - j) * dsc(2))
+                drawLine(0f, 0f, 0f, size * dsc(0), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBLRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBreakLineRotLeft(scale, w, h, paint)
+}
