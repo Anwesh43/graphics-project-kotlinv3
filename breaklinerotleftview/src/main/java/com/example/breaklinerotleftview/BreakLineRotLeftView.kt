af98b3bb9dcdc01a320305c430f761dfef55270f
Anwesh43 <anweshthecool0@gmail.com>
2023-10-05 11:25:23 +0530

    breaklinerotleftview: adding fixes

--- breaklinerotleftview/src/main/java/com/example/breaklinerotleftview/BreakLineRotLeftView.kt
+++ breaklinerotleftview/src/main/java/com/example/breaklinerotleftview/BreakLineRotLeftView.kt
@@ -41,7 +41,7 @@ fun Canvas.drawBreakLineRotLeft(scale : Float, w : Float, h : Float, paint : Pai
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
         for (j in 0..1) {
             drawXY(0f, 0f) {
                 rotate(rot * j * dsc(1) - (180 + rot) * (1 - j) * dsc(2))
