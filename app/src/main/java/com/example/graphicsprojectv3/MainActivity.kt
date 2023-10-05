e956e9c4015f06c92dd3036e382fb4cdfa7783f5
Anwesh43 <anweshthecool0@gmail.com>
2023-10-05 11:24:56 +0530

    breaklinerotleftview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.rightlinerotrightview.RightLineRotRightView
+import com.example.breaklinerotleftview.BreakLineRotLeftView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        RightLineRotRightView.create(this)
+        BreakLineRotLeftView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
