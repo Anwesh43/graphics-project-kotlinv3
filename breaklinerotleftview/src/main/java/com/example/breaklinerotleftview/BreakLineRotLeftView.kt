53d7cd1257856f8ae2640779af17d793f6af40a8
Anwesh43 <anweshthecool0@gmail.com>
2023-10-05 11:02:17 +0530

    breaklinerotleftview: adding BLRLNode class

--- breaklinerotleftview/src/main/java/com/example/breaklinerotleftview/BreakLineRotLeftView.kt
+++ breaklinerotleftview/src/main/java/com/example/breaklinerotleftview/BreakLineRotLeftView.kt
@@ -122,4 +122,45 @@ class BreakLineRotLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLRLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLRLNode? = null
+        private var prev : BLRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLRLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLRLNode {
+            var curr : BLRLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
