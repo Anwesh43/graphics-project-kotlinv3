9d019a515b895d5796e0d147846a209148d9f561
Anwesh43 <anweshthecool0@gmail.com>
2023-10-06 11:53:23 +0530

    fullcircletohalfarcview: adding FCTHANode class

--- fullcircletohalfarcview/src/main/java/com/example/fullcircletohalfarcview/FullCircleToHalfArcView.kt
+++ fullcircletohalfarcview/src/main/java/com/example/fullcircletohalfarcview/FullCircleToHalfArcView.kt
@@ -122,4 +122,45 @@ class FullCircleToHalfArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class FCTHANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : FCTHANode? = null
+        private var prev : FCTHANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = FCTHANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFCTHANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FCTHANode {
+            var curr : FCTHANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
