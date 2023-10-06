5bb684b343581cdac8305d6b605294340b022abb
Anwesh43 <anweshthecool0@gmail.com>
2023-10-06 11:27:19 +0530

    fullcircletohalfarcview: adding FullCircleToHalfArcView class

--- fullcircletohalfarcview/src/main/java/com/example/fullcircletohalfarcview/FullCircleToHalfArcView.kt
+++ fullcircletohalfarcview/src/main/java/com/example/fullcircletohalfarcview/FullCircleToHalfArcView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawFCTHANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawFullCircleToHalfArc(scale, w, h, paint)
 }
+
+class FullCircleToHalfArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
