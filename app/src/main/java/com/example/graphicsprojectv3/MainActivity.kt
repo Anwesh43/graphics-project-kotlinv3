886697a5f41f2f4cb84243bef79f7658b79240a8
Anwesh43 <anweshthecool0@gmail.com>
2023-10-06 12:24:06 +0530

    fullcircletohalfarcview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.breaklinerotleftview.BreakLineRotLeftView
+import com.example.fullcircletohalfarcview.FullCircleToHalfArcView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        BreakLineRotLeftView.create(this)
+        FullCircleToHalfArcView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
