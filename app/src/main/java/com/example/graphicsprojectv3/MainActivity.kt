be67e8258ecb5c03cb40c2b292f71e8b09037bfa
Anwesh43 <anweshthecool0@gmail.com>
2023-10-07 15:39:41 +0530

    sqbentlinerightview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.fullcircletohalfarcview.FullCircleToHalfArcView
+import com.example.sqbentlinerightview.SqBentLineRightView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        FullCircleToHalfArcView.create(this)
+        SqBentLineRightView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
