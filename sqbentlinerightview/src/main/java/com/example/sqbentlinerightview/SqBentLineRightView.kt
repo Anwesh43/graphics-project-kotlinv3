a3e23f8dc673f7cc570b9e700bd0d7824ebaa891
Anwesh43 <anweshthecool0@gmail.com>
2023-10-07 15:39:02 +0530

    sqbentlinerightview: adding static create method

--- sqbentlinerightview/src/main/java/com/example/sqbentlinerightview/SqBentLineRightView.kt
+++ sqbentlinerightview/src/main/java/com/example/sqbentlinerightview/SqBentLineRightView.kt
@@ -215,4 +215,13 @@ class SqBentLineRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : SqBentLineRightView {
+            val view : SqBentLineRightView = SqBentLineRightView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
