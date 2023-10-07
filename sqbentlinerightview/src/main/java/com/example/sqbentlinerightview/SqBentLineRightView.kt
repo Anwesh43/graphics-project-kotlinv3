706d54a3fcf9f3aebc0d2ff982da601f1e5d0b72
Anwesh43 <anweshthecool0@gmail.com>
2023-10-07 12:18:10 +0530

    sqbentlinerightview: adding Canvas extension methods

--- sqbentlinerightview/src/main/java/com/example/sqbentlinerightview/SqBentLineRightView.kt
+++ sqbentlinerightview/src/main/java/com/example/sqbentlinerightview/SqBentLineRightView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -30,3 +30,36 @@ val deg : Float = 45f
 fun Int.inverse() = 1f / this
 fun Float.maxScale(i : Int, n : Int) = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqBentLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawRect(RectF(-size, 0f, 0f, size * dsc(0)), paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(-deg * dsc(3))
+            drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+        }
+    }
+}
+
+fun Canvas.drawSBLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawSqBentLineRot(scale, w, h, paint)
+}
