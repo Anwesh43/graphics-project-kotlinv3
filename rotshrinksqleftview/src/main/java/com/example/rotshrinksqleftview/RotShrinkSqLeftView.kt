905fd6ced1078cf4d41ee7054201a2e2ee3a1e52
Anwesh43 <anweshthecool0@gmail.com>
2023-10-08 12:45:04 +0530

    rotshrinksqleftview: adding RotShirnkSqLeftView class

--- rotshrinksqleftview/src/main/java/com/example/rotshrinksqleftview/RotShrinkSqLeftView.kt
+++ rotshrinksqleftview/src/main/java/com/example/rotshrinksqleftview/RotShrinkSqLeftView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawRSLNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawRotSqLeft(scale, w, h, paint)
 }
+
+class RotShrinkSqLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
