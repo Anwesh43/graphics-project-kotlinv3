758f573aa29045db5a214fc6707101112d08ec9c
Anwesh43 <anweshthecool0@gmail.com>
2023-10-09 10:09:40 +0530

    ulinerotcatchsqview: adding ULRCSNode class

--- uplinerotcatchsqview/src/main/java/com/example/uplinerotcatchsqview/UpLineRotCatchSqView.kt
+++ uplinerotcatchsqview/src/main/java/com/example/uplinerotcatchsqview/UpLineRotCatchSqView.kt
@@ -124,4 +124,45 @@ class UpLineRotCatchSqView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ULRCSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ULRCSNode? = null
+        private var prev : ULRCSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ULRCSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawULRCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ULRCSNode {
+            var curr : ULRCSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
