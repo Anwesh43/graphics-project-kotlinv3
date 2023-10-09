5addccf7dae96bccb1861eb692dfc0ae8972fbf0
Anwesh43 <anweshthecool0@gmail.com>
2023-10-09 10:18:34 +0530

    ulinerotcatchsqview: adding view in activity

--- app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
+++ app/src/main/java/com/example/graphicsprojectv3/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.graphicsprojectv3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.rotshrinksqleftview.RotShrinkSqLeftView
-
+import com.example.uplinerotcatchsqview.UpLineRotCatchSqView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        RotShrinkSqLeftView.create(this)
+        UpLineRotCatchSqView.create(this)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
