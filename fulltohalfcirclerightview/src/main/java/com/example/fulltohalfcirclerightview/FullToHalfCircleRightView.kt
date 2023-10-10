8df2fd2d7019bb2af038d8b8bbce49a8c5cde392
Anwesh43 <anweshthecool0@gmail.com>
2023-10-10 10:26:23 +0530

    fulltohalfcirclerightview: adding FullToHalfCircleRightView class

--- fulltohalfcirclerightview/src/main/java/com/example/fulltohalfcirclerightview/FullToHalfCircleRightView.kt
+++ fulltohalfcirclerightview/src/main/java/com/example/fulltohalfcirclerightview/FullToHalfCircleRightView.kt
@@ -52,3 +52,19 @@ fun Canvas.drawFTHRNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawFullToHalfRight(scale, w, h, paint)
 }
+
+class FullToHalfCircleRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
