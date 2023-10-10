8a7cb38d93299e513160d69d7eb9446059440dc0
Anwesh43 <anweshthecool0@gmail.com>
2023-10-10 10:31:29 +0530

    fulltohalfcirclerightview: adding FTHCRNode class

--- fulltohalfcirclerightview/src/main/java/com/example/fulltohalfcirclerightview/FullToHalfCircleRightView.kt
+++ fulltohalfcirclerightview/src/main/java/com/example/fulltohalfcirclerightview/FullToHalfCircleRightView.kt
@@ -46,7 +46,7 @@ fun Canvas.drawFullToHalfRight(scale : Float, w : Float, h : Float, paint : Pain
     }
 }
 
-fun Canvas.drawFTHRNode(i : Int, scale : Float, paint : Paint) {
+fun Canvas.drawFTCHRNode(i : Int, scale : Float, paint : Paint) {
     val w : Float = width.toFloat()
     val h : Float = height.toFloat()
     paint.color = Color.parseColor(colors[i])
@@ -113,6 +113,47 @@ class FullToHalfCircleRightView(ctx : Context) : View(ctx) {
             if (animated) {
                 animated = false
             }
+        }
+    }
+
+    data class FTHCRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : FTHCRNode? = null
+        private var prev : FTHCRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = FTHCRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFTCHRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FTHCRNode {
+            var curr : FTHCRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
