82b5ee5938480666c892ec92ea75c137345b9c07
Anwesh43 <anweshthecool0@gmail.com>
2023-10-10 10:32:44 +0530

    fulltohalfcirclerightview: adding FullTOHalfCircle class

--- fulltohalfcirclerightview/src/main/java/com/example/fulltohalfcirclerightview/FullToHalfCircleRightView.kt
+++ fulltohalfcirclerightview/src/main/java/com/example/fulltohalfcirclerightview/FullToHalfCircleRightView.kt
@@ -156,4 +156,27 @@ class FullToHalfCircleRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class FullToHalfCircle(var i : Int) {
+
+        private var curr : FTHCRNode = FTHCRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
